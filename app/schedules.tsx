b530c3e22faab3d2c8003143bffd49efb61903ee
karolinascsp <karolinasc.sp@gmail.com>
2025-03-12 20:08:34 -0300

    quarta

--- app/schedules.tsx
+++ app/schedules.tsx
@@ -1,29 +1,38 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+import { View, Text, StyleSheet, TextInput, Button, TextInputBase } from 'react-native';
 
 
 export default function ScheduleScreen(){
 // aqui é typescript
+    const [discipline, setDiscipline] = useState('');
+
 
     return (
         <View>
             {/* aqui é typescript dentro do front */}
             <text>Meu Cronograma</text>
             <View style={style.row}>  
-                <View style={style.form}>
+                    <View style={style.form}>
                      <TextInput placeholder="Id"  />   
                      <TextInput placeholder="Url"  /> 
                      <TextInput placeholder="UserId"  />
                      <TextInput placeholder="Class"  />
-                     <TextInput placeholder="Discipline"  />
-                     
+                     <TextInput 
+                        placeholder="Discipline"  
+                        value={discipline} 
+                        onChangeText={setDiscipline}
+                    />
+ 
                      <TextInput placeholder="Location"  />
                      <TextInput placeholder="Start_time"  />
                      <TextInput placeholder="End_time"  />
-                    
-                     <Button title= "Salvar Cronograma" />
-                    
+                     
+                     <Button title="Salvar Cronograma" color="green" />
 
+                    {discipline}
+                    
+                     
+                    
                 </View>
                
             </View>
