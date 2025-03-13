be24e76ab979f0d1c06487b67dd64780afe8fc52
MarciusMcflay <jhorge.marcius@gmail.com>
2025-03-12 22:06:52 -0300

    passado use state

--- app/posts.tsx
+++ app/posts.tsx
@@ -1,8 +1,33 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput } from 'react-native';
+import { View, Text, TextInput, Button } from 'react-native';
 
 export default function PostScreen() {
-    const [description, setDescription] = useState('');
+    const [req, setReq] = useState({
+        description: '',
+        url: '',
+        id: 0,
+        createAt: new Date().toISOString(),
+        userId: 0,
+    });
+
+    const [posts, setPosts ] = useState<{
+        description: string,
+        url: string,
+        id: number,
+        createAt: string,
+        userId: number,
+    }[]>([])
+
+    function handleRegister(){
+        setPosts([...posts, req])
+        setReq({
+            description: '',
+            url: '',
+            id: 0,
+            createAt: new Date().toISOString(),
+            userId: 0,
+        })
+    }
 
     return (
         <View>
@@ -10,9 +35,19 @@ export default function PostScreen() {
 
             <TextInput
                 placeholder="Digite a descrição"
-                value={description}
-                onChangeText={setDescription}
+                value={req.description}
+                onChangeText={ (t) => setReq({...req, description: t}) }
             />
+            {req.description}
+
+            <TextInput
+                placeholder="Digite a url"
+                value={req.url}
+                onChangeText={ (t) => setReq({...req ,url: t }) }
+            />
+            {req.url}
+
+            <Button title='CADASTRAR' onPress={ handleRegister } />
         </View>
     );
 }
