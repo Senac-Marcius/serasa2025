31465572d1c0e432ef7c00abc3ac1c0655c820ff
Camargo2030 <victor.camargo02@gmail.com>
2025-03-12 21:13:03 -0300

    iniciada tela de cadastro do objeto

--- app/classes.tsx
+++ app/classes.tsx
@@ -3,7 +3,14 @@ import {View, Text, StyleSheet, TextInput, Button } from 'react-native';
 
 export default function ClassScreen() {
 
-
+    const [Req, setReq] = useState({
+        name: '',
+        id: 0,
+        date: '',
+        start_date: '',
+        end_date: '',       
+        createAt: new Date(),
+    });
 
 
 
@@ -15,19 +22,44 @@ export default function ClassScreen() {
                 <View style={styles.row}>
                     <View style={styles.form}>
                         <TextInput
-                        placeholder="Nome da Turma"/>
+                        placeholder="Nome da Turma"
+                        value={Req.name}
+                        onChangeText={(text) => setReq({...Req, name: text})}
+                        />
+                        {Req.name}
                         <TextInput
-                        placeholder="Quantidade de Alunos"/>
+                        placeholder="Quantidade de Alunos"
+                        value={Req.quant}
+                        onChangeText={(text) => setReq({...Req, quant: text})}
+                        />
+                        {Req.quant}
                         <TextInput
-                        placeholder="ID da Turma"/>
+                        placeholder="ID da Turma"
+                        value={Req.id}
+                        onChangeText={(text) => setReq({...Req, id: text})}
+                        />
+                        {Req.id}
                         <TextInput
-                        placeholder="Data de criação da Turma"/>
+                        placeholder="Data de criação da Turma"
+                        value={Req.date}
+                        onChangeText={(text) => setReq({...Req, date: text})}
+                        />
+                        {Req.date}
                         <TextInput
-                        placeholder="Data de Inicio"/>
+                        placeholder="Data de Inicio"
+                        value={Req.start_date}
+                        onChangeText={(text) => setReq({...Req, start_date: text})}
+                        />
+                        {Req.start_date}
                         <TextInput
-                        placeholder="Data de Finalização"/>
+                        placeholder="Data de Finalização"
+                        value={Req.end_date}
+                        onChangeText={(text) => setReq({...Req, end_date: text})}
+                        
+                        />
+                        {Req.end_date}
 
-                        <Button title="Cadastrar Turma"/>
+                        <Button title="Cadastrar Turma" color= 'purple'/>
 
                     </View>
                     
