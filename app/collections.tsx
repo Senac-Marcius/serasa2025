4da33863a80ff54c300fbc444d1c6ab1ee90508b
Nikoly-Ap <snikoly19@gmail.com>
2025-03-13 18:40:32 -0300

    req button

--- app/collections.tsx
+++ app/collections.tsx
@@ -1,26 +1,57 @@
 import React, {useState} from 'react';
 import{View, Text, StyleSheet, FlatList, TextInput,Button} from 'react-native';
+import { text } from 'stream/consumers';
+ //função userState só retorna para uma variavel const
 
-    export default function CollectionScreen(){
-        
+export default function CollectionScreen(){
+    const [req, setReq] = useState({
+        id:0,
+        name:'',
+        quantity:'',
+        star:'',  
+        creatAt: new Date().toISOString()
+    });
+    const [collections,setCollections] = useState<{
+        name:string,
+        quantity:string, 
+        star:string, 
+        creatAt:string, 
+        id:number,}[]>([])
 
-
+    function handleRegister(){
+        setCollections ([...collections,req])
+    }
 
         return (//encapsulamento 
             <View>{/* modo de comentar, aqui é typescript dentro do front */}
-                <Text>Minha tela de acervo</Text>
+                <Text>Tela de acervo</Text>
                 <View style= {style.row}>
                     <View style= {style.form}>
                         <TextInput
                         placeholder="Nome"
+                            value={req.name}
+                            onChangeText={(text)=>setReq({...req ,name:text})} //
                         />
+                        {req.name}
+
                          <TextInput
                         placeholder="Quantidade"
+                            value={req.quantity}
+                            onChangeText={(text)=>setReq({...req ,quantity:text})}
                         />
+                        {req.quantity}
+
                          <TextInput
                         placeholder="Estrelas"
+                            value={req.star}
+                            onChangeText={(text)=>setReq({...req ,star:text})}
                         />
-                         <Button title='Cadastrar'/>
+                        {req.star}
+
+                        <Button title='Cadastrar'
+                        onPress={handleRegister}
+                        color="pink"
+                        />
                     </View>
                 </View>
             </View>
@@ -45,6 +76,5 @@ import{View, Text, StyleSheet, FlatList, TextInput,Button} from 'react-native';
             shadowRadius: 5,
 
         },
-
     })
    
