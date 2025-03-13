c55eb6b10619764ddcf2583084fd9849d204f3ce
Gustavo-gzus <gustavojesussilva800@gmail.com>
2025-03-12 22:08:57 -0300

    Criação de vetor para cadastrar os dados no banco e limpar os campos que foram inseridos.

--- app/products.tsx
+++ app/products.tsx
@@ -5,21 +5,55 @@ import {View, Text, StyleSheet, TextInput, Button} from 'react-native';
 export default function productScreen(){
 //aqui é typescript
 
+    const [req, setReq] = useState({
+        description:'',
+        name:'',
+        id: 0,
+        createAt:  new Date().toISOString(),
+        userId: 0,
+    });
+
+    const [products, setProducts] = useState<{ 
+        description: string,
+        name: string,
+        id: number,
+        userId: number,
+        createAt: string
+        }[]>([]);
+
+        function handleRegister(){  
+            setProducts([...products, req]) 
+            setReq({
+                description:'',
+                name:'',
+                id: 0,
+                createAt:  new Date().toISOString(),
+                userId: 0,})  
+        }
 
     return (
-            <View>
+        <View>
                 {/*aqui é typescript dentro do front */ }
                 <Text>
                     Minha tela dos Produtos</Text>
                     <View style={styles.row}>
-                        <View style={styles.form} style={styles.Button}>
+                        <View style={styles.form}>
+                            
                             <TextInput
-                            placeholder="descrição"
+                            placeholder="Digite o nome"
+                            value={req.name}
+                            onChangeText={(text)=> setReq({...req, name: text})}
                             />
+                            {req.name}
                             <TextInput
-                            placeholder="nome"
+                            placeholder="Digite a descrição" 
+                            value={req.description}
+                            onChangeText={(text)=> setReq({...req, description: text})}
                             />
-                            <Button title="Cadastrar" onPress={() => {}} />
+                            {req.description}
+
+                            <Button color="purple" title="Cadastrar" onPress={handleRegister} />
+
                         </View>
                         
                     </View> 
@@ -46,7 +80,7 @@ const styles = StyleSheet.create({
         shadowRadius: 5,
     },
     Button: {
-        backgroundColor: '#fff',
+        backgroundColor:'#fff',
         padding: 20,
         borderRadius: 8,
         boxShadow: '0 2px 4px rgba(0,0,0,0.1)',
