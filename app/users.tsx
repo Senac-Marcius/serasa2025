fc2d174d2b6e0b8e6bd82218ce2d91ecae0c2f90
jhennyy <jhennifermirarchi@gmail.com>
2025-03-12 22:02:23 -0300

    criação req e button

--- app/users.tsx
+++ app/users.tsx
@@ -1,46 +1,104 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, FlatList, TextInput, Button} from 'react-native';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, Button } from 'react-native';
 
 
 export default function UserScreen() {
+    const [req, setReq] = useState({
+        name: '',
+        password: '',
+        cpf: '',
+        age: '',
+        contact: '',
+        email: '',
+        address: '',
+        createAt: new Date().toISOString(),
+        userId: 0
+
+    });
+
+    const [users, setUsers] = useState<{
+        //tipo, tipo vetor, objeto
+        name: string,
+        password: string,
+        cpf: string,
+        age: string,
+        contact: string,
+        email: string,
+        address: string,
+        createAt: string,
+        userId: number
+    }[]>([])
+
+    function handleRegister(){
+        setUsers([...users, req])
+        
+
+    }
+
     //criar outras funções, sempre retorna html
     //aqui é typescript
-
-
     return (
         <View>
             <Text>Minha tela de usuários</Text>
             <View style={styles.row}>
                 <View style={styles.form}>
+
                     <TextInput
-                        placeholder="Nome do usuário:"
+                        placeholder="Nome:"
+                        value={req.name} //O atributo value vincula o campo de texto ao estado name
+                        onChangeText={(text) => setReq({ ...req, name: text })} //Ele recebe o texto digitado pelo usuário como argumento e o passa para a função setName
+
                     />
+                    {req.name}
+
+
+
 
                     <TextInput
                         placeholder="Senha:"
+                        value={req.password} //O atributo value vincula o campo de texto ao estado name
+                        onChangeText={(text) => setReq({ ...req, password: text })} //Ele recebe o texto digitado pelo usuário como argumento e o passa para a função setName
+
                     />
+                    {req.password}
 
                     <TextInput
                         placeholder="CPF:"
+                        value={req.cpf} //O atributo value vincula o campo de texto ao estado name
+                        onChangeText={(text) => setReq({ ...req, cpf: text })}
                     />
+                    {req.cpf}
 
                     <TextInput
                         placeholder="Idade:"
+                        value={req.age} //O atributo value vincula o campo de texto ao estado name
+                        onChangeText={(text) => setReq({ ...req, age: text })}
                     />
+                    {req.age}
 
                     <TextInput
                         placeholder="Contato:"
+                        value={req.contact} //O atributo value vincula o campo de texto ao estado name
+                        onChangeText={(text) => setReq({ ...req, contact: text })}
                     />
+                    {req.contact}
 
                     <TextInput
                         placeholder="Email:"
+                        value={req.email} //O atributo value vincula o campo de texto ao estado name
+                        onChangeText={(text) => setReq({ ...req, email: text })}
+
                     />
+                    {req.email}
 
                     <TextInput
                         placeholder="Endereço:"
+                        value={req.address} //O atributo value vincula o campo de texto ao estado name
+                        onChangeText={(text) => setReq({ ...req, address: text })}
                     />
+                    {req.address}
 
-                    <Button title='Cadastrar'/>
+                    <Button title='CADASTRAR' color="purple" onPress={ handleRegister} />
 
 
                 </View>
@@ -72,5 +130,18 @@ const styles = StyleSheet.create({
         shadowRadius: 5,
     },
 
+    button: {
+        backgroundColor: 'purple',
+        paddingVertical: 12,
+        borderRadius: 20,
+        alignItems: 'center',
+        marginTop: 20,
+
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
 
 })
