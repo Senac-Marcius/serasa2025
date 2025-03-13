9dbd4b396048fe8289f1fe370cf7317200bf0b3a
SthefaneA <sthegarzon85@gmail.com>
2025-03-12 22:10:41 -0300

    sthefane

--- app/calendar.tsx
+++ app/calendar.tsx
@@ -3,22 +3,49 @@ import {View, Text, StyleSheet, TextInput, Button} from 'react-native';
  
 export default function CalendarScreen(){
 //aqui é typescript
+    const [req,setreq] = useState({
+        studentname: '',
+        course: '',
+        registrationdate: '',
+        period: '',
+        id: 0,
+        creadAt: new Date().toISOString(),
 
+});
+
+const [calendar, setCalendars] = useState<{studentname:string, course: string, registrationdate: string, period:string, id:number,  }[]>([])
+
+function handleRegister(){ //apaga o estado de req para um objeto padrão e adiciona um novo objeto 
+    setCalendars([...calendar,req])
+    setreq({studentname: '',
+        course: '',
+        registrationdate: '',
+        period: '',
+        id: 0,
+        creadAt: new Date().toISOString(), 
+    })
+}
 
 return (
     <View>
-        Olá Mundo 
+        Olá Aluno! 
         {/*aqui é typescript dentro do front*/}
-        <Text>Minha tela das postagens</Text>
+        <Text>Tela de Cronograma</Text>
         <View style={styles.row}>
             <View style ={styles.form}>
                 
-                <TextInput placeholder="Nome do aluno:"/>
-                <TextInput placeholder="Curso:"/>
-                <TextInput placeholder="Data da Matrícula:"/>
-                <TextInput placeholder="Período:"/>
+                <TextInput placeholder="Nome do aluno:" value={req.studentname} onChangeText={(text) => setreq({... req , studentname: text }) } />
+                {req.studentname} 
+                <TextInput placeholder="Curso:" value={req.course} onChangeText={(text) => setreq({... req , course:text }) } />
+                {req.course} 
+                <TextInput placeholder="Data da Matrícula:"value={req.registrationdate} onChangeText={(text) => setreq({... req , registrationdate:text }) } />
+                {req.registrationdate} 
+                <TextInput placeholder="Período:"value={req.period} onChangeText={(text) => setreq({... req , period:text }) } />
+                {req.period}   
 
-            <Button title='Acessar'/>
+            <Button title='Acessar' onPress={ handleRegister } />
+
+        
             </View> 
             
         </View> 
@@ -47,5 +74,6 @@ const styles = StyleSheet.create({
         shadowRadius: 5,
     },
 
+    
 
 })
