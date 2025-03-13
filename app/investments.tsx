950bd2b0efde71becd36647911910ab4cd4c8860
guilherme jesus silva <gulhermejesus6@gmail.com>
2025-03-12 22:10:06 -0300

    useState/onChangeText

--- app/investments.tsx
+++ app/investments.tsx
@@ -1,22 +1,74 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TextInput, Button,  } from 'react-native';
+import { View, Text, StyleSheet, TextInput, Button, } from 'react-native';
 
 export default function investmentScreen(){
  //aqui é typescript   
+    const [req, setReq] = useState({
+        description: '',
+        name: '',
+        url: '',
+        id: 0,
+        createAt: new Date().toISOString(),
+        userId: '',
+        value: '',
+    });
+
+    const [investments, setInvestments] = useState<{
+        description: string,
+        url: string,
+        name: string,
+        id: number,
+        createAt: string,
+        userId: string,
+    }[]>([]);
+
+    function handleRegister(){
+        setInvestments([...investments, req]);
+        setReq({
+            description: '',
+            name: '',
+            url: '',
+            id: 0,
+            createAt: new Date().toISOString(),
+            userId: '',
+            value: '',
+        });
+    
+    }
 
     return (
       <View>
         {/* Aqui é typescript dentro do fron */}
-        <Text>Minha tela das postagens</Text>
+        <Text>Meus Investimentos</Text>
         <View style={styles.row}>
             <View style={styles.form}>
                 <TextInput
                     placeholder='Descrição'
+                    value={req.description}
+                    onChangeText={(text) => setReq({...req, description: text })}
                 />
                 <TextInput
-                    placeholder='url'
+                    placeholder='Nome'
+                    value={req.name}
+                    onChangeText={(text) => setReq({...req, name: text })}
                 />
-                <Button title='Cadastrar' /> 
+                <TextInput
+                    placeholder='URL'
+                    value={req.url}
+                    onChangeText={(text) => setReq({...req, url: text })}
+                />
+                <TextInput
+                    placeholder='Valor'
+                    value ={req.value}
+                    onChangeText={(text) => setReq({...req, value: text })}
+                />
+                <TextInput
+                    placeholder='UserId'
+                    value={req.userId}
+                    onChangeText={(text) => setReq({...req, userId: text })}
+                />
+
+                <Button title='Cadastrar' onPress={ handleRegister }/> 
             </View>
             
         </View>
