1274b5d6f1b12d307022011f7bad62530ef12de5
Lorrany <79610401+lollyblackthorn@users.noreply.github.com>
2025-03-13 22:28:55 -0300

    Estamos formatando o print, faltou formatar os inputs e deixar estilizado.

--- app/record.tsx
+++ app/record.tsx
@@ -1,9 +1,9 @@
 import React, {useState} from 'react';//cria uma var que att sozinha qualquer obj que criarmos dentro dela 
-import { View, Text, StyleSheet, FlatList, TextInput, Button, ScrollView, TouchableOpacity } from 'react-native';//como se fosse a div do html, ele agrupa as coisas 
+import { View, Text, StyleSheet, FlatList, TextInput, Button, ScrollView, } from 'react-native';//como se fosse a div do html, ele agrupa as coisas 
 
 export default function RecordScreen(){//função que será exportada
     const[req,setReq] = useState({
-
+        id:0,
         name: '',
         email: '',                   
         rg: '',
@@ -13,6 +13,7 @@ export default function RecordScreen(){//função que será exportada
     })
 
     const [records, setRecord] = useState<{
+        id: number,
         name: string,
         email: string,                  
         rg: string,
@@ -22,8 +23,9 @@ export default function RecordScreen(){//função que será exportada
     }[]>([])
 
     function handleRegister(){
-        setRecord([...records, req])
+        setRecord([...records, req, ])
         setReq({
+            id: 0,
             name: '',
             email: '',                   
             rg: '',
@@ -31,6 +33,7 @@ export default function RecordScreen(){//função que será exportada
             cpf: '',                   
             createAt: new Date().toString(),
         })
+        
     }
 
 
@@ -42,21 +45,40 @@ export default function RecordScreen(){//função que será exportada
             <Text style = {styles.title}>Solicitação de Documentos</Text>
             <View style = {styles.row}>{/* variavel.parametro da Function */}
                 <View style = {styles.form}>
-                    <TextInput aria-label='Nomeee' placeholder= "Nome" value={req.name} onChangeText= { (text) => setReq({...req,name: text})}/>
-                    {req.name}
+                    <TextInput placeholder= "Nome" value={req.name} onChangeText= { (text) => setReq({...req,name: text})}/>
+                    
                     <TextInput placeholder= "Email" value={req.email} onChangeText= { (text) => setReq({...req,email: text})}/>
-                    {req.email}
+                    
                     <TextInput placeholder="RG" value={req.rg} onChangeText={(text) => setReq({ ...req, rg: text.replace(/[^0-9]/g, '') })} keyboardType="numeric"/>
-                    {req.rg}
+                    
                     <TextInput placeholder= "Data de Nascimento" value={req.dateBirth} onChangeText= { (text) => setReq({...req,dateBirth: text})}/>
-                    {req.dateBirth}
+                    
                     <TextInput placeholder="CPF" value={req.cpf} onChangeText={(text) => setReq({ ...req, cpf: text.replace(/[^0-9]/g, '') })} keyboardType="numeric"/>
-                    {req.cpf}
+                    
 
-                    <Button title='CADASTRAR' onPress={ handleRegister }/>
-
-   
+                    <Button title='CADASTRAR'  onPress={ handleRegister }/>
+                        
                 </View>
+                <ScrollView>
+                    <FlatList
+                        data={records}
+                        keyExtractor={(item) => item.id.toString()}
+                        renderItem={({item}) => (
+                            <View style = {styles.itemContainer}>
+                                <Text>Nome: {item.name}</Text>
+                                <Text>Email: {item.email}</Text>
+                                <Text>RG: {item.rg}</Text>
+                                <Text>Data de Nascimento: {item.dateBirth}</Text>
+                                <Text>CPF: {item.cpf}</Text>
+                                <Text>Criado em:{item.createAt}</Text>
+
+                            </View>                            
+                        )}
+                        showsVerticalScrollIndicator={true}
+                        contentContainerStyle={{ paddingBottom: 50 }}
+                    
+                    />
+                </ScrollView>
                 
 
             </View>
@@ -65,10 +87,21 @@ export default function RecordScreen(){//função que será exportada
 }
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20,
+        backgroundColor: '#fff',
+    },
+    itemContainer: {
+        backgroundColor: "#F2F2F2",
+        padding: 10,
+        marginVertical: 5,
+        borderRadius: 8,
+      },
     row: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'flex-start',
+        flexDirection: 'row', 
+        justifyContent: 'space-between', 
+        alignItems: 'flex-start', 
     },
     form: {
         flex: 1,//primeiro item
@@ -80,6 +113,22 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.1,
         shadowOffset: { width: 0, height: 4 },
         shadowRadius: 5,
+        paddingBottom: 20,
+    },
+    formContainer: {
+        flex: 1,
+        marginRight: 10,
+        padding: 20,
+        backgroundColor: '#F2F2F2',
+        borderRadius: 10,
+        shadowColor: '#000',
+        shadowOpacity: 0.1,
+        shadowOffset: { width: 0, height: 4 },
+        shadowRadius: 5,
+    },
+    listContainer: {
+        flex: 1, 
+        padding: 10,
     },
     title: {
         fontSize: 22,
@@ -87,23 +136,31 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         marginBottom: 20,
     },
-    button: {
-        backgroundColor: '#007bff',
-        paddingVertical: 12,
-        paddingHorizontal: 20,
-        borderRadius: 8,
-        alignItems: 'center',
-        marginTop: 15,
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.2,
-        shadowRadius: 3,
-        elevation: 5, // Sombra no Android
+    /*subtitle: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginBottom: 10,
     },
-    buttonText: {
-        color: '#fff',
+    input: {
+        borderWidth: 1,
+        borderColor: '#ccc',
+        borderRadius: 5,
+        padding: 10,
+        marginBottom: 10,
+    },
+    postItem: {
+        padding: 10,
+        marginVertical: 5,
+        backgroundColor: '#f8f8f8',
+        borderRadius: 5,
+    },
+    postText: {
         fontSize: 16,
         fontWeight: 'bold',
     },
-
+    postUrl: {
+        fontSize: 14,
+        color: '#007BFF',
+        marginBottom: 5,
+    },*/
 });
