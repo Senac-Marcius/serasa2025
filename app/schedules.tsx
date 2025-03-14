2ed01b902d7ce85777be5a49978e37f591c70a6b
karolinascsp <karolinasc.sp@gmail.com>
2025-03-13 22:28:37 -0300

    quinta.k

--- app/schedules.tsx
+++ app/schedules.tsx
@@ -1,46 +1,97 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TextInput, Button, TextInputBase } from 'react-native';
+import { View, Text, StyleSheet, TextInput, Button, TextInputBase, FlatList } from 'react-native';
 
 
 export default function ScheduleScreen(){
 // aqui é typescript
-    const [discipline, setDiscipline] = useState('');
+    const [req, setReq] = useState({
+            id: '',
+            url: '',
+            class: '',
+            userId: 0,
+            discipline: '',
+            location: '',
+            start_time: '',
+            end_time: '',
+            createAt: new Date().toISOString(),
+        
+    });
 
+    const [schedules, setSchedules] = useState<{
+        id: string,
+        url: string,
+        class: string,
+        userId: number,
+        discipline: string,
+        location: string,
+        start_time: string,
+        end_time: string,
+        createAt: string,
+      
+    }[]>([]);
+
+    function handleRegister() {
+        setSchedules([...schedules, req])
+        setReq({
+            id: req.id + 1,
+            url: '',
+            class: '',
+            userId: 0,
+            discipline: '',
+            location: '',
+            start_time: '',
+            end_time: '',
+            createAt: new Date().toISOString(),
+        })
+    }
 
     return (
         <View>
+        
             {/* aqui é typescript dentro do front */}
             <text>Meu Cronograma</text>
-            <View style={style.row}>  
-                    <View style={style.form}>
-                     <TextInput placeholder="Id"  />   
-                     <TextInput placeholder="Url"  /> 
-                     <TextInput placeholder="UserId"  />
-                     <TextInput placeholder="Class"  />
-                     <TextInput 
-                        placeholder="Discipline"  
-                        value={discipline} 
-                        onChangeText={setDiscipline}
-                    />
- 
-                     <TextInput placeholder="Location"  />
-                     <TextInput placeholder="Start_time"  />
-                     <TextInput placeholder="End_time"  />
-                     
-                     <Button title="Salvar Cronograma" color="green" />
+            <View style={styles.row}>
+                <View style={styles.form}>
 
-                    {discipline}
-                    
+                     <TextInput placeholder="url" value={req.url} onChangeText={(text) => setReq({...req, url:text})} /> 
+                     <TextInput placeholder="class" value={req.class} onChangeText={(text) => setReq({...req, class:text})} /> 
+                     <TextInput placeholder="discipline" value={req.discipline} onChangeText={(text) => setReq({...req, discipline:text})} /> 
+                     <TextInput placeholder="location" value={req.location} onChangeText={(text) => setReq({...req, location:text})} /> 
+                     <TextInput placeholder="start_time" value={req.start_time} onChangeText={(text) => setReq({...req, start_time:text})} /> 
+                     <TextInput placeholder="end_time" value={req.end_time} onChangeText={(text) => setReq({...req, end_time:text})} /> 
                      
-                    
+                     <Button title="CADASTRAR" onPress={ handleRegister } color="purple" />
                 </View>
-               
-            </View>
+
+                <FlatList 
+                    data={schedules}
+                    keyExtractor={ (item) => item.id.toString()}
+                    renderItem={({item}) => (
+                        <View
+                        style={styles.card}
+                        > 
+                            
+                            
+
+
+                            <text style={styles.form} > {item.discipline} </text>
+                            <text> {item.userId} </text>
+                            <text> {item.url} </text>
+                            <text> {item.class} </text>
+                            <text> {item.location} </text>
+                            <text> {item.start_time} </text>
+                            <text> {item.end_time} </text>
+                            <text> {item.createAt} </text>
+                        </View>
+                    )}
+                />
+                
+            </View>   
         </View>
-    );
+        )             
 }
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
     row: {
         flexDirection: 'row',
         justifyContent: 'space-between',
@@ -56,12 +107,25 @@ const style = StyleSheet.create({
         shadowOpacity: 0.1,
         shadowOffset: { width: 0, height: 4 },
         shadowRadius: 5,
-    },
+        },
+
+       card: {
+        flex: 1,
+        marginRight: 10,
+        padding: 40,
+        backgroundColor: '#F2F2F2',
+        borderRadius: 10,
+        shadowColor: '#000',
+        shadowOpacity: 0.1,
+        shadowOffset: { width: 0, height: 4 },
+        shadowRadius: 5,
+        marginBottom: 20,
+
+        },
+
 
 
 })
-
-
 
 
 
