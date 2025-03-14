bf259983dd9d888d578d690416d43df2161e08ab
SthefaneA <sthegarzon85@gmail.com>
2025-03-13 22:27:06 -0300

    Code text

--- app/calendar.tsx
+++ app/calendar.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {View, Text, StyleSheet, TextInput, Button} from 'react-native';
+import {View, Text, StyleSheet, TextInput, Button, FlatList} from 'react-native';
  
 export default function CalendarScreen(){
 //aqui é typescript
@@ -35,21 +35,37 @@ return (
             <View style ={styles.form}>
                 
                 <TextInput placeholder="Nome do aluno:" value={req.studentname} onChangeText={(text) => setreq({... req , studentname: text }) } />
-                {req.studentname} 
+                
                 <TextInput placeholder="Curso:" value={req.course} onChangeText={(text) => setreq({... req , course:text }) } />
-                {req.course} 
+                
                 <TextInput placeholder="Data da Matrícula:"value={req.registrationdate} onChangeText={(text) => setreq({... req , registrationdate:text }) } />
-                {req.registrationdate} 
+                
                 <TextInput placeholder="Período:"value={req.period} onChangeText={(text) => setreq({... req , period:text }) } />
-                {req.period}   
+                   
 
             <Button title='Acessar' onPress={ handleRegister } />
 
         
             </View> 
-            
+            <View style={styles.listContainer}>
+            <FlatList
+                data={calendar}
+                keyExtractor= { (item) => item.id. toString ()}
+                
+                renderItem={({item})=> (
+                    <View style ={styles.list}>
+                    
+                        <Text style ={styles.code}>Nome do aluno:  {item.studentname}</Text>
+                        <Text style ={styles.code}>Curso: {item.course}</Text>
+                        <Text style ={styles.code}>Data da matricula: {item.registrationdate}</Text>
+                        <Text style ={styles.code}>Periodo: {item.period}</Text>
+                    
+                   
+                    </View>
+                )}
+        />
+        </View>
         </View> 
-
     </View>
 
     );
@@ -73,7 +89,29 @@ const styles = StyleSheet.create({
         shadowOffset: { width: 0, height: 4 },
         shadowRadius: 5,
     },
+ 
+    list: {
+        flex: 1,
+        marginRight: 10,
+        padding: 20,
+        marginBottom: 10,
+        borderWidth: 1, 
+        backgroundColor: '#F2F2F2',
+        borderRadius: 10,
+        shadowColor: '#000',
+        shadowOpacity: 0.1,
+        shadowOffset: { width: 0, height: 4 },
+        shadowRadius: 5,
+    },
 
+    listContainer: {
+        flex: 1,
+    },
     
-
+    code:{
+        fontSize:14,
+        marginBottom: 4,
+        
+    }
+    
 })
