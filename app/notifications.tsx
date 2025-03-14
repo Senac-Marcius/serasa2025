782886386453fcea9e58281b5931a765674fd876
iris1518 <neuslinda12@gmail.com>
2025-03-13 22:31:42 -0300

    caixinha

--- app/notifications.tsx
+++ app/notifications.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TextInput, Button} from 'react-native';
+import { View, FlatList, Text, StyleSheet, TextInput, Button} from 'react-native';
 
 
 export default function NotificationScreen(){
@@ -44,33 +44,50 @@ return (
         <View style={styles.row}>
             <View style={styles.form}>
                 <TextInput
-                        placeholder = "nome"
+                        placeholder = "Nome:"
                         value={req.name}
                         onChangeText={(text) => setReq({...req ,name: text  })}
                  />
                 <TextInput 
-                        placeholder = "descrição"  
+                        placeholder = "Descrição:"  
                         value={req.description}
                         onChangeText={(text) => setReq({...req ,description: text  })}
                 />
                 <TextInput 
-                placeholder = "classificação"  
+                placeholder = "classificação:"  
                 value={req.classification}
                 onChangeText={(text) => setReq({...req ,classification: text  })}
                 />
 
-                <TextInput placeholder = "url" 
+                <TextInput placeholder = "Url:" 
                          value={req.url}
                          onChangeText={(text) => setReq({...req ,url: text  })}
                 />
-               
-                {req.name}
-
-                <Button title="cadastrar" onPress={handleRegister}/>
+-+
+                <Button title="cadastrar:" onPress={handleRegister}/>
 
             </View>
+
+           <FlatList
+                    data={notifications}
+                    keyExtractor={(item) => item.id.toString()}
+                    renderItem={({item}) => (
+                        <View style={styles.notificationStyle}>
+
+                           <text> Nome: {item.name}</text> 
+                           <text> Descrição: {item.description}</text>
+                           <text> Url: {item.url}</text>
+                           <text> Classificação: {item.classification}</text>
+                           <text> UserId: {item.userId}</text>
+                           <text> CreatAt: {item.creatAt}</text>
+
+                        </View>
+
+                    )}
+           />
            
         </View>
+
     </View>
 );
 }
@@ -93,4 +110,18 @@ const styles = StyleSheet.create({
         shadowRadius: 5,
     },
 
+    notificationStyle:{
+    flex: 1,
+    marginRight: 10,
+    padding: 20,
+    backgroundColor: '#F2F2F2',
+    borderRadius: 15,
+    shadowColor: '#000',
+    shadowOpacity: 0.1,
+    shadowOffset: { width: 0, height: 4 },
+    shadowRadius: 5,
+    margin: 10, 
+    width: 400,
+
+    },
 })
