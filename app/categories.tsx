0627145fd8c164beb7493a02cda454f94f89988f
aneliza-caleran <aneliza-caleran>
2025-03-13 22:33:30 -0300

    categoria

--- app/categories.tsx
+++ app/categories.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'; 
-import { View, Text,StyleSheet, FlatList, TextInput, Button  } from 'react-native';
+import { View, Text,StyleSheet, FlatList, TextInput, Button,  } from 'react-native';
 
 export default function CategoryScreen(){
     const [req, setReq] = useState({
@@ -7,12 +7,18 @@ export default function CategoryScreen(){
         description:'', 
         category:'',
         color:'',
+        id:0,
+        createAt: new Date ().toISOString(),
+        userId: 0,
     });
     const [categories, setCategories] = useState<{
         name: string,
         description:string, 
         category:string,
         color:string,
+        id: number
+        createAt: string,
+        userId: number
     }[]>([]);
 
     function handleRegister(){
@@ -22,6 +28,9 @@ export default function CategoryScreen(){
             description:'', 
             category:'',
             color:'',
+            id:0,
+            createAt: new Date ().toISOString(),
+            userId: 0,
         })
     }
 
@@ -34,31 +43,43 @@ export default function CategoryScreen(){
                 <View  style={styles.form}>
                     <TextInput placeholder="Nome"
                         value={req.name}
-                        onChangeText={(text) => setReq({...req,name:text})}
+                        onChangeText={(text) => setReq({...req, name:text})}
                     />
-                    {req.name}
 
                     <TextInput placeholder="Descrição"
                          value={req.description}
-                         onChangeText={(text) => setReq({...req,description:text})}
+                         onChangeText={(text) => setReq({...req, description:text})}
                     />
-                    {req.description}
 
                     <TextInput placeholder="Categoria"
                        value={req.category}
-                       onChangeText={(text) => setReq({...req,category:text})}
-                    />
-                    {req.category}
+                       onChangeText={(text) => setReq({...req, category:text})}
+                    />                   
 
                     <TextInput placeholder="cor"
                         value={req.color}
-                        onChangeText={(text) => setReq({...req,color:text})}
+                        onChangeText={(text) => setReq({...req, color:text})}
                     />
-                    {req.color}
 
                      <Button title="Cadastrar" onPress={handleRegister}/> 
-                    
                 </View>
+
+                <FlatList
+                    data={categories}
+                    keyExtractor={ ( item ) => item.id.toString() }
+                    renderItem={({ item }) => (
+                        <View style={styles.categorieStyle}>
+
+                            <Text>{item.name}</Text>
+                            <Text>{item.description}</Text>
+                            <Text>{item.category}</Text>
+                            <Text>{item.createAt}</Text>
+
+                        </View> 
+                   
+                    )}
+                />
+
             </View>
             
         </View>
@@ -76,11 +97,23 @@ const styles = StyleSheet.create({
         flex: 1,
         marginRight: 10,
         padding: 20,
-        backgroundColor: '#F2F2F2',
+        backgroundColor: '#faeeff ',
         borderRadius: 10,
         shadowColor: '#000',
         shadowOpacity: 0.1,
         shadowOffset: { width: 0, height: 4 },
         shadowRadius: 5,
     },
+    categorieStyle:{
+        flex: 1,
+        marginRight: 20,
+        padding: 20,
+        backgroundColor: '#faeeff ',
+        borderRadius: 10,
+        shadowColor: '#000',
+        shadowOpacity: 5.1,
+        shadowOffset: { width: 0, height: 1 },
+        shadowRadius: 5,
+        margin: 10
+    }
 })
