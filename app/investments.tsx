a0e334797e275984c4feaa5d911a88e05fdb1919
guilherme jesus silva <gulhermejesus6@gmail.com>
2025-03-14 22:30:07 -0300

    commit

--- app/investments.tsx
+++ app/investments.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TextInput, Button, FlatList, } from 'react-native';
+import { View, Text, StyleSheet, TextInput, Button, FlatList, TouchableOpacity } from 'react-native';
 
 export default function investmentScreen(){
  //aqui é typescript   
@@ -7,11 +7,12 @@ export default function investmentScreen(){
         description: '',
         name: '',
         url: '',
-        id: 0,
+        id: -1,
         createAt: new Date().toISOString(),
         userId: '',
         value: '',
     });
+
 
     const [investments, setInvestments] = useState<{
         description: string,
@@ -24,19 +25,41 @@ export default function investmentScreen(){
     
 
     function handleRegister(){
-        setInvestments([...investments, req]);
+        if(req.id == -1){
+            const nId = investments.length ? investments[investments.length - 1].id + 1 : 0;
+            const newInvestment = { ...req, id: nId };
+
+            setInvestments([...investments, newInvestment]);
+        }else{
+            setInvestments(investments.map(i => (i.id == req.id) ? req : i));
+        }
+
         setReq({
             description: '',
             name: '',
             url: '',
-            id: req.id + 1,
+            id: -1,
             createAt: new Date().toISOString(),
             userId: '',
             value: '',
         });
-    
     }
 
+    function editInvestment(id:number){
+        const investment = investments.find(i => i.id == id)
+        if(investment)
+        setReq(investment)
+    }
+
+
+    function delInvestment(id:number){
+        const list = investments.filter(i => i.id != id)
+        if(list)
+            setInvestments(list)
+
+    }
+    
+    
     return (
       <View>
         {/* Aqui é typescript dentro do front */}
@@ -76,13 +99,23 @@ export default function investmentScreen(){
                 data={investments}
                 keyExtractor={ (item) => item.id.toString() }
                 renderItem={({item}) => (
-                    <view style={styles.form}>
-                       <Text> {item.description}</Text>
-                       <Text> {item.name}</Text>
-                       <Text> {item.url}</Text>
-                       <Text> {item.createAt}</Text>
-                       <Text> {item.userId}</Text>
-                        </view>
+                    <View style={styles.item}>
+                       <Text> Descrição: {item.description}</Text>
+                       <Text> Nome: {item.name}</Text>
+                       <Text> Url: {item.url}</Text>
+                       <Text> Data: {item.createAt}</Text>
+                       <Text> ID de Usuario: {item.userId}</Text>
+
+                        <View style={styles.buttons}>
+                            <TouchableOpacity 
+                            style={styles.editButton} onPress={ () => { editInvestment(item.id) } }><Text style={styles.editButtonText}> editar</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity
+                            style={styles.delButton} onPress={ () => { delInvestment(item.id) } }> <Text style={styles.delButtonText}> excluir</Text>
+                            </TouchableOpacity>
+                        </View>
+
+                        </View> 
                 )}
             />
         </View>
@@ -96,6 +129,12 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between', 
         alignItems: 'flex-start'
     }, 
+    buttons: {
+        flexDirection: 'row',  
+        alignContent: 'space-between', 
+        alignItems: 'center',
+        gap: 10,
+    },
     form : {
         flex: 1,
         marginRight: 10,
@@ -107,4 +146,47 @@ const styles = StyleSheet.create({
         shadowOffset: { width: 0, height: 4 },
         shadowRadius: 20,
     },
-})
+    item : {
+        flex: 1,
+        marginRight: 10,
+        padding: 20,
+        backgroundColor: '#F2F2F2',
+        borderRadius: 10,
+        shadowColor: '#000',
+        shadowOpacity: 0.1,
+        shadowOffset: { width: 0, height: 4 },
+        shadowRadius: 20,
+        marginBottom: 20,
+    },
+
+    editButton : {
+        backgroundColor: '#FFD700',
+        padding: 10,
+        borderRadius: 10,
+        shadowColor: '#000',
+        shadowOpacity: 0.1,
+        shadowOffset: { width: 0, height: 4 },
+        shadowRadius: 20,
+    },
+
+    editButtonText: {
+        color: '#000',
+        fontWeight: 'bold',
+    },
+
+    delButton : {
+        backgroundColor: '#FF0000',
+        padding: 10,
+        borderRadius: 10,
+        shadowColor: '#000',
+        shadowOpacity: 0.1,
+        shadowOffset: { width: 0, height: 4 },
+        shadowRadius: 20,
+    },    
+
+    delButtonText: {
+        color: '#000',
+        fontWeight: 'bold',
+    }
+
+});
