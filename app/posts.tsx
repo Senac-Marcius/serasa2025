f41690b33b1a48d0ec2ba00eb2397376f7dc35a1
MarciusMcflay <jhorge.marcius@gmail.com>
2025-03-14 21:40:19 -0300

    feito o crud novamente

--- app/posts.tsx
+++ app/posts.tsx
@@ -1,11 +1,12 @@
+import { List } from '@ui-kitten/components';
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
 
 export default function PostScreen() {
     const [req, setReq] = useState({
         description: '',
         url: '',
-        id: 0,
+        id: -1,
         createAt: new Date().toISOString(),
         userId: 0,
     });
@@ -19,35 +20,130 @@ export default function PostScreen() {
     }[]>([])
 
     function handleRegister(){
-        setPosts([...posts, req])
-        setReq({
+        if(req.id == -1){
+            const newId = posts.length ? posts[posts.length - 1].id + 1 : 0;
+            const newPost = { ...req, id: newId };
+            
+            setPosts([...posts, newPost]);
+        }else{
+            setPosts(posts.map(jTNL =>  (jTNL.id == req.id)? req : jTNL  ));
+        }
+
+        setReq({ 
+            id: -1,
             description: '',
             url: '',
-            id: 0,
             createAt: new Date().toISOString(),
             userId: 0,
         })
     }
 
+    function editPost(id:number){
+        const post = posts.find(p => p.id == id)
+        if(post)
+            setReq(post)
+    }
+
+    function delPost(id:number){
+        const list = posts.filter(p => p.id != id)
+        setPosts(list)
+    }
+
     return (
         <View>
-            <Text>Tela de Postagens</Text>
+            <Text>Minha tela de Posts</Text>
+            <View style={styles.row}>
+                <View style={styles.form}>
 
-            <TextInput
-                placeholder="Digite a descrição"
-                value={req.description}
-                onChangeText={ (t) => setReq({...req, description: t}) }
-            />
-            {req.description}
+                    <TextInput
+                        placeholder="Digite a descrição"
+                        value={req.description}
+                        onChangeText={ (t) => setReq({...req, description: t}) }
+                    />
 
-            <TextInput
-                placeholder="Digite a url"
-                value={req.url}
-                onChangeText={ (t) => setReq({...req ,url: t }) }
-            />
-            {req.url}
 
-            <Button title='CADASTRAR' onPress={ handleRegister } />
+                    <TextInput
+                        placeholder="Digite a url"
+                        value={req.url}
+                        onChangeText={ (t) => setReq({...req ,url: t }) }
+                    />
+
+                    <Button title='CADASTRAR' onPress={ handleRegister } />
+                </View>
+
+                <FlatList 
+                    style={styles.list}
+                    data={posts}
+                    keyExtractor={ (item) => item.id.toString() }
+                    renderItem={({ item }) => (
+                        <View style={styles.item}>
+                                <Text>Description: {item.description} </Text>
+                                <Text>Url: {item.url} </Text>
+                                <Text>Data de Criação: {item.createAt} </Text>
+                                <Text>Userid: {item.userId} </Text>
+
+                                <View style={styles.buttonsContanier}>
+                                    <TouchableOpacity onPress={ () => { editPost(item.id) } } >EDIT</TouchableOpacity>
+                                    <TouchableOpacity onPress={ () => { delPost(item.id) } }>DELETE</TouchableOpacity>
+                                </View>
+                        </View>
+                    )}
+                />
+            </View>
         </View>
+
     );
 }
+
+const styles = StyleSheet.create({
+    row:{
+        flexDirection: 'row',
+        alignItems: 'flex-start',
+        alignContent: 'space-between',
+    },
+    form:{
+        flex: 1,
+        width: '98%',
+        padding: 20,
+        backgroundColor: '#fff',
+        borderRadius: 10,
+
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 4 },
+        shadowOpacity: 0.2,
+        shadowRadius: 5,
+
+        elevation: 5,
+    },
+    list:{
+        flex: 1,
+        margin: 10,
+        marginTop: 0
+    },
+    item:{
+        alignItems: 'flex-start',
+        alignContent: 'space-between',
+
+        width: '98%',
+        padding: 20,
+        backgroundColor: '#fff',
+        borderRadius: 10,
+        marginBottom: 10,
+
+        shadowColor: '#000',
+        shadowOffset: { width: 2, height: 2 },
+        shadowOpacity: 0.2,
+        shadowRadius: 4,
+
+        elevation: 5,
+    },
+    label:{
+        color: '#ff0000',
+    },
+    buttonsContanier:{
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 20,
+        alignContent: 'space-around',        
+    }
+});
