94c6cc7d7cba4528d83049eff6523f7a6644aa83
PedroSouto543 <pedrospsouto@gmail.com>
2025-03-14 22:29:01 -0300

    touchableopacity e .map

--- app/expenses.tsx
+++ app/expenses.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TextInput, Button, FlatList} from 'react-native' ;
+import { View, Text, StyleSheet, TextInput, Button, FlatList,TouchableOpacity, } from 'react-native' ;
 
 export default function ExpenseScreen(){
 // aqui é typescript
@@ -7,7 +7,7 @@ export default function ExpenseScreen(){
             name: '',
             url: '',
             description: '',
-            id: 0,
+            id: -1,
             cost: '',
             creatAt : new Date(). toISOString(),
             userId: 0,
@@ -23,23 +23,43 @@ export default function ExpenseScreen(){
     }[]>([]) 
 
     function handleRegister(){
+        if(req.id == -1){
+            const newid = expense.length ? expense[postMessage.length - 1].id + 1 :0;
+            const newExpense = {...req, id:newid};
+            setExpense([...expense, newExpense]);
+        }else{
+            setExpense(expense.map(e => (e.id == req.id ? req : e)));
+        }
+
         setReq({
             name: '',
             url: '',
             description: '',
-            id: 0,
+            id: -1,
             cost: '',
             creatAt : new Date(). toISOString(),
             userId: 0,
         });
-        setExpense([...expense, req]);
+       
         
+    }
+
+    function editExpense(id:number){
+        const expenses = expense.find(e => e.id == id )
+        if(expenses)
+            setReq(expenses)
+    }
+
+    function delExpense(id:number){
+        const list = expense.filter(e => e.id != id)
+        if(list)
+            setExpense(list)
     }
 
     return (
         <View>
             {/* aqui é typecript dentro do front */}
-            <Text>tela de despesas</Text>
+            <Text style={styles.title}>tela de despesas</Text>
             <View style={styles.row}>
                 <View style={styles.form}>
                     <TextInput 
@@ -66,7 +86,7 @@ export default function ExpenseScreen(){
                         onChangeText ={(text) => setReq({...req ,cost: text}) }
                     />
 
-                    <Button title='Cadastrar' onPress= { handleRegister }/>
+                    <TouchableOpacity style={styles.buttonRegister} onPress= { handleRegister }>Cadastrar</TouchableOpacity>
                 </View>
 
                 <FlatList
@@ -74,11 +94,27 @@ export default function ExpenseScreen(){
                     keyExtractor={(item) => item.id.toString()}
                     renderItem={({item}) => (
                         <View style={styles.card} >
-                          <Text style={styles.name} >{item.name}</Text>
-                          <Text style={styles.url} >{item.url}</Text> 
-                          <Text style={styles.description} >{item.description}</Text>  
-                          <Text style={styles.cost} >{item.cost}</Text> 
-                          <Text style={styles.userId} >{item.userId}</Text>  
+                            <Text style={styles.textlis} >{item.name}</Text>
+                            <Text style={styles.textlis} >{item.url}</Text> 
+                            <Text style={styles.textlis} >{item.description}</Text>  
+                            <Text style={styles.textlis} >{item.cost}</Text> 
+                            <Text style={styles.textlis} >{item.userId}</Text>
+                            
+                            <View style= {styles.buttonsContainer}>
+
+                                <TouchableOpacity
+                                 style={styles.editButton}
+                                  onPress={() => {editExpense(item.id)}}
+                                  >Edit
+                                  </TouchableOpacity>
+
+                                <TouchableOpacity
+                                 style={styles.delButton}
+                                  onPress={()=>{delExpense(item.id)}}
+                                  >Delete
+                                  </TouchableOpacity>
+
+                            </View> 
                         </View>
                     )}
                 /> 
@@ -107,7 +143,7 @@ const styles = StyleSheet.create({
     card: {
         backgroundColor: "#f8f9fa",
         padding: 16,
-        marginVertical: 8,
+        marginBottom: 8,
         marginHorizontal: 12,
         borderRadius: 8,
         shadowColor: "#000",
@@ -116,27 +152,63 @@ const styles = StyleSheet.create({
         shadowRadius: 4,
         elevation: 3, // Sombra para Android
       },
-      name: {
-        fontSize: 18,
-        fontWeight: "bold",
-        color: "#333",
-      },
-      url: {
-        fontSize: 14,
-        color: "#1E90FF",
-      },
-      description: {
+      textlis: {
         fontSize: 14,
         color: "#555",
         marginVertical: 4,
       },
-      cost: {
-        fontSize: 16,
-        fontWeight: "bold",
-        color: "#28a745",
+
+      buttonRegister:{
+        backgroundColor: "#ab66f9",
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+        borderRadius: 5,
+        color: '#ffffff'
       },
-      userId: {
-        fontSize: 12,
-        color: "#888",
+      
+      buttonsContainer:{
+       
+        textAlign: 'center',
+        fontSize: 20,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        gap: 20,
+        alignContent: 'space-between',
+        
       },
+      editButton: {
+        backgroundColor: "#9a47f8",
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+        borderRadius: 5,
+        color: '#ffffff'
+     },
+     delButton:{
+        backgroundColor: "#36046e",
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+        borderRadius: 5,
+        color: '#ffffff'
+     },
+     title:{
+        marginBottom: 8,
+        fontSize: 30,
+        fontWeight: "bold", 
+        textAlign: "center",
+        backgroundColor: "#ab66f9",
+        borderRadius: 5,
+        color:'#ffffff',
+        letterSpacing: 1.5,
+        textTransform: "uppercase",
+        textShadowColor: "rgba(0, 0, 0, 0.2)",
+        fontStyle: "italic",
+     },
+     titleBase: {
+        backgroundColor: "#222", 
+        paddingVertical: 12, 
+        paddingHorizontal: 24,
+        borderRadius: 8, 
+      },
+     
     });
