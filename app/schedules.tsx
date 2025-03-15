12f3d0ed942cbb7f038fd161a316f9178fcf3a67
karolinascsp <karolinasc.sp@gmail.com>
2025-03-14 21:53:57 -0300

    sexta.k

--- app/schedules.tsx
+++ app/schedules.tsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TextInput, Button, TextInputBase, FlatList } from 'react-native';
+import { View, Text, StyleSheet, TextInput, Button, TextInputBase, FlatList, TouchableOpacity } from 'react-native';
 
 
 export default function ScheduleScreen(){
 // aqui é typescript
     const [req, setReq] = useState({
-            id: '',
+            id: -1,
             url: '',
             class: '',
             userId: 0,
@@ -18,7 +18,7 @@ export default function ScheduleScreen(){
     });
 
     const [schedules, setSchedules] = useState<{
-        id: string,
+        id: number,
         url: string,
         class: string,
         userId: number,
@@ -31,9 +31,16 @@ export default function ScheduleScreen(){
     }[]>([]);
 
     function handleRegister() {
-        setSchedules([...schedules, req])
-        setReq({
-            id: req.id + 1,
+        if(req.id == -1){
+            const newId = schedules.length ? schedules[schedules.length -1].id + 1 : 0;
+            const newSchedule = {...req, id: newId};
+
+            setSchedules([...schedules, newSchedule]);
+        }else{
+            setSchedules(schedules.map(s =>(s.id == req.id) ? req : s));
+        }
+        setReq({ 
+            id: -1,
             url: '',
             class: '',
             userId: 0,
@@ -44,6 +51,18 @@ export default function ScheduleScreen(){
             createAt: new Date().toISOString(),
         })
     }
+    
+    function editSchedules(id:number){
+        const schedule = schedules.find(s => s.id == id) 
+        if(schedule)
+        setReq(schedule)
+    }
+
+    function delSchedules(id:number){
+        const list = schedules.filter(s => s.id != id)
+            setSchedules(list)
+    }
+
 
     return (
         <View>
@@ -53,12 +72,12 @@ export default function ScheduleScreen(){
             <View style={styles.row}>
                 <View style={styles.form}>
 
-                     <TextInput placeholder="url" value={req.url} onChangeText={(text) => setReq({...req, url:text})} /> 
-                     <TextInput placeholder="class" value={req.class} onChangeText={(text) => setReq({...req, class:text})} /> 
-                     <TextInput placeholder="discipline" value={req.discipline} onChangeText={(text) => setReq({...req, discipline:text})} /> 
-                     <TextInput placeholder="location" value={req.location} onChangeText={(text) => setReq({...req, location:text})} /> 
-                     <TextInput placeholder="start_time" value={req.start_time} onChangeText={(text) => setReq({...req, start_time:text})} /> 
-                     <TextInput placeholder="end_time" value={req.end_time} onChangeText={(text) => setReq({...req, end_time:text})} /> 
+                     <TextInput placeholder="Digite a url:" value={req.url} onChangeText={(text) => setReq({...req, url:text})} /> 
+                     <TextInput placeholder="Digite a classe:" value={req.class} onChangeText={(text) => setReq({...req, class:text})} /> 
+                     <TextInput placeholder="Digite a disciplina:" value={req.discipline} onChangeText={(text) => setReq({...req, discipline:text})} /> 
+                     <TextInput placeholder="Digite a localização:" value={req.location} onChangeText={(text) => setReq({...req, location:text})} /> 
+                     <TextInput placeholder="Digite o horário de início:" value={req.start_time} onChangeText={(text) => setReq({...req, start_time:text})} /> 
+                     <TextInput placeholder="Digite o horário do fim:" value={req.end_time} onChangeText={(text) => setReq({...req, end_time:text})} /> 
                      
                      <Button title="CADASTRAR" onPress={ handleRegister } color="purple" />
                 </View>
@@ -67,21 +86,26 @@ export default function ScheduleScreen(){
                     data={schedules}
                     keyExtractor={ (item) => item.id.toString()}
                     renderItem={({item}) => (
-                        <View
-                        style={styles.card}
-                        > 
-                            
-                            
+                        <View style={ styles.item}>
+                        
+                         
+                            <Text> {item.discipline} </Text>
+                            <Text> {item.userId} </Text>
+                            <Text> {item.url} </Text>
+                            <Text> {item.class} </Text>
+                            <Text> {item.location} </Text>
+                            <Text> {item.start_time} </Text>
+                            <Text> {item.end_time} </Text>
+                            <Text> {item.createAt} </Text>  
+               
+                            <View style={styles.buttonsContanier}>
+                                <TouchableOpacity style={styles.buttonedit} onPress={ () => { editSchedules (item.id) } } >EDIT</TouchableOpacity>
+                                <TouchableOpacity style={styles.buttondel} onPress={() => {delSchedules (item.id)}}>DELETE</TouchableOpacity>
+
+                            </View>
+                                
 
 
-                            <text style={styles.form} > {item.discipline} </text>
-                            <text> {item.userId} </text>
-                            <text> {item.url} </text>
-                            <text> {item.class} </text>
-                            <text> {item.location} </text>
-                            <text> {item.start_time} </text>
-                            <text> {item.end_time} </text>
-                            <text> {item.createAt} </text>
                         </View>
                     )}
                 />
@@ -92,10 +116,20 @@ export default function ScheduleScreen(){
 }
 
 const styles = StyleSheet.create({
+    buttonsContanier: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 20, 
+        alignContent: 'space-around',
+        padding: 20,
+        borderRadius: 10,
+    },
+
+
     row: {
         flexDirection: 'row',
         justifyContent: 'space-between',
-        alignItems: 'flex-start'
+        alignItems: 'flex-start',
     },
     form: {
         flex: 1,
@@ -109,7 +143,7 @@ const styles = StyleSheet.create({
         shadowRadius: 5,
         },
 
-       card: {
+       item: {
         flex: 1,
         marginRight: 10,
         padding: 40,
@@ -120,11 +154,27 @@ const styles = StyleSheet.create({
         shadowOffset: { width: 0, height: 4 },
         shadowRadius: 5,
         marginBottom: 20,
-
+        
         },
 
+        buttonedit: {
+            alignItems: 'center',
+            padding: 20,
+            borderRadius: 10,
+            backgroundColor: 'purple',
+            textAlign: 'center',
+        },
+        
+        buttondel:{
+            alignItems: 'center',
+            padding: 20,
+            borderRadius: 10,
+            backgroundColor: 'gray',
+            textAlign: 'center',
+           
+            
 
-
+        },
 })
 
 
