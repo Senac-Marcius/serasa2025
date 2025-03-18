381379f69ed11b1a4193c9e33c407dbd0ab46874
Lorrany <79610401+lollyblackthorn@users.noreply.github.com>
2025-03-17 21:49:54 -0300

    Falta inserir o edit e fazer ele funcionar, c+v no del e ajustar a func

--- app/record.tsx
+++ app/record.tsx
@@ -1,161 +1,156 @@
-import React, {useState} from 'react';//cria uma var que att sozinha qualquer obj que criarmos dentro dela 
-import { View, Text, StyleSheet, FlatList, TextInput, Button, ScrollView, TouchableOpacity } from 'react-native';//como se fosse a div do html, ele agrupa as coisas 
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, FlatList, StyleSheet, ScrollView } from 'react-native';
 
-export default function RecordScreen(){//função que será exportada
-    const[req,setReq] = useState({
-        id:-1,
-        name: '',
-        email: '',                   
-        rg: '',
-        dateBirth:'',
-        cpf: '',                   
-        createAt: new Date().toString(),
-    })
 
-    const [records, setRecord] = useState<{//cria o vetor records e coloca as informações nele
-        id: number,
-        name: string,
-        email: string,                  
-        rg: string,
-        dateBirth:string,
-        cpf: string,                   
-        createAt: string,
-    }[]>([])
+export default function RecordScreen() {
+    // Estados individuais para os inputs
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [rg, setRg] = useState('');
+    const [dateBirth, setDateBirth] = useState('');
+    const [cpf, setCpf] = useState('');
 
-    function handleRegister(){
 
-        /*if (!name.trim() || !email.trim() || !rg.trim() || !dateBirth.trim() || !cpf.trim()) {
+    // Estado para armazenar os registros
+    const [records, setRecords] = useState<{ 
+        id: number; 
+        name: string; 
+        email: string; 
+        rg: string; 
+        dateBirth: string; 
+        cpf: string; 
+        createAt: string; 
+    }[]>([]);
+
+
+    // Função para adicionar um novo registro
+    const handleRegister = () => {
+        if (!name.trim() || !email.trim() || !rg.trim() || !dateBirth.trim() || !cpf.trim()) {
             alert('Preencha todos os campos!');
             return;
-        }*/
-
-        if(req.id == -1){
-            const newId = records.length ? records[records.length - 1].id+1:0;
-            const newPost = {...req, id: newId};
-            setRecord([...records, newPost])//passa os dados de um novo registro em req para o vetor records
-        }else{
-            setRecord(records.map(item => (item.id == req.id ? req : item)));
         }
 
-        setReq({//reseta os campos
-            id: -1,
-            name: '',
-            email: '',                   
-            rg: '',
-            dateBirth:'',
-            cpf: '',                   
+
+        const newRecord = {
+            id: records.length ? records[records.length - 1].id + 1 : 1,
+            name,
+            email,
+            rg,
+            dateBirth,
+            cpf,
             createAt: new Date().toString(),
-        })
-        
-    }
-    const editRecord = (id: number) => {
-        const recordAtt = records.find(item => item.id == id)
-        if(recordAtt)
-            setReq(records);
+        };
+
+
+        setRecords([...records, newRecord]);
+
+
+        // Resetando os campos
+        setName('');
+        setEmail('');
+        setRg('');
+        setDateBirth('');
+        setCpf('');
     };
 
+
+    // Função para excluir um registro
     const deleteRecord = (id: number) => {
-        const recordIndex = records.filter(item => item.id != id)
-        if(records)
-            setReq(recordIndex);
+        setRecords(records.filter((record) => record.id !== id));
     };
-
 
 
     return (
-        <View>
-            {/* isso é um comentário em typescrispt dentro do front */}
-
-            <Text style = {styles.title}>Solicitação de Documentos</Text>
-            <View style = {styles.row}>{/* variavel.parametro da Function */}
-                <View style = {styles.form}>
-                    <TextInput style={styles.input} placeholder= "Nome" value={req.name} onChangeText= { (text) => setReq({...req,name: text})}/>
+        <View style={styles.container}>
+            <View style={styles.row}>
+                <View style={styles.formContainer}>
+                    <Text style={styles.title}>Solicitação de Documentos</Text>
                     
-                    <TextInput style={styles.input} placeholder= "Email" value={req.email} onChangeText= { (text) => setReq({...req,email: text})}/>
-                    
-                    <TextInput style={styles.input} placeholder="RG" value={req.rg} onChangeText={(text) => setReq({ ...req, rg: text.replace(/[^0-9]/g, '') })} keyboardType="numeric"/>
-                    
-                    <TextInput style={styles.input} placeholder= "Data de Nascimento" value={req.dateBirth} onChangeText= { (text) => setReq({...req,dateBirth: text})}/>
-                    
-                    <TextInput style={styles.input} placeholder="CPF" value={req.cpf} onChangeText={(text) => setReq({ ...req, cpf: text.replace(/[^0-9]/g, '') })} keyboardType="numeric"/>
-                    
-
-                    <Button title='CADASTRAR'  onPress={ handleRegister }/>
-                        
-                </View>
-                <ScrollView style={{flex:1}}>
-                    <FlatList
-                        data={records}
-                        keyExtractor={(item) => item.id.toString()}
-                        renderItem={({item}) => (
-                            <View style = {styles.itemContainer}>
-                                <Text>Nome: {item.name}</Text>
-                                <Text>Email: {item.email}</Text>
-                                <Text>RG: {item.rg}</Text>
-                                <Text>Data de Nascimento: {item.dateBirth}</Text>
-                                <Text>CPF: {item.cpf}</Text>
-                                <Text>Criado em:{item.createAt}</Text>
-
-                                <View style = {styles.buttonsContainer} >                                    
-                                    <TouchableOpacity style={styles.button} onPress={()=>{editRecord(item.id)}}>
-                                        <Text style={styles.buttonText}>EDIT</Text>
-                                    </TouchableOpacity>
-                                    <TouchableOpacity style={styles.button} onPress={()=>{deleteRecord(item.id)}}>
-                                        <Text style={styles.buttonText}>DELETE</Text>
-                                    </TouchableOpacity>
-                                </View>
- 
-                            </View> 
-                                                       
-                        )}
-                        showsVerticalScrollIndicator={true}
-                        contentContainerStyle={{ paddingBottom: 50 }}
-                   
+                    <TextInput 
+                        style={styles.input} 
+                        placeholder="Nome" 
+                        value={name} 
+                        onChangeText={setName}
                     />
-                </ScrollView>
-                
 
+
+                    <TextInput 
+                        style={styles.input} 
+                        placeholder="Email" 
+                        value={email} 
+                        onChangeText={setEmail}
+                    />
+
+
+                    <TextInput 
+                        style={styles.input} 
+                        placeholder="RG" 
+                        value={rg} 
+                        onChangeText={(text) => setRg(text.replace(/[^0-9]/g, ''))} 
+                        keyboardType="numeric"
+                    />
+
+
+                    <TextInput 
+                        style={styles.input} 
+                        placeholder="Data de Nascimento" 
+                        value={dateBirth} 
+                        onChangeText={setDateBirth}
+                    />
+
+
+                    <TextInput 
+                        style={styles.input} 
+                        placeholder="CPF" 
+                        value={cpf} 
+                        onChangeText={(text) => setCpf(text.replace(/[^0-9]/g, ''))} 
+                        keyboardType="numeric"
+                    />
+
+
+                    <Button title="Cadastrar" onPress={handleRegister} />
+                </View>
+
+
+                <View style={styles.listContainer}>
+                    <Text style={styles.subtitle}>Registros Cadastrados</Text>
+                    <ScrollView style={{ flex: 1 }}>
+                        <FlatList
+                            data={records}
+                            keyExtractor={(item) => item.id.toString()}
+                            renderItem={({ item }) => (
+                                <View style={styles.recordItem}>
+                                    <Text style={styles.recordText}>Nome: {item.name}</Text>
+                                    <Text style={styles.recordText}>Email: {item.email}</Text>
+                                    <Text style={styles.recordText}>RG: {item.rg}</Text>
+                                    <Text style={styles.recordText}>Data de Nascimento: {item.dateBirth}</Text>
+                                    <Text style={styles.recordText}>CPF: {item.cpf}</Text>
+                                    <Text style={styles.recordText}>Criado em: {item.createAt}</Text>
+                                    <Button title="Excluir" color="red" onPress={() => deleteRecord(item.id)} />
+                                </View>
+                            )}
+                            showsVerticalScrollIndicator={true}
+                            contentContainerStyle={{ paddingBottom: 20 }}
+                        />
+                    </ScrollView>
+                </View>
             </View>
         </View>
     );
 }
 
+
+// Estilos
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         padding: 20,
         backgroundColor: '#fff',
     },
-    itemContainer: {
-        backgroundColor: "#F2F2F2",
-        padding: 10,
-        marginVertical: 5,
-        borderRadius: 8,
-      },
-    buttonsContainer: {
-        flexDirection: 'row',
-        alignItems:'center',
-        justifyContent: 'space-between',
-        marginTop: 10
-        
-      },
-
     row: {
         flexDirection: 'row', 
         justifyContent: 'space-between', 
         alignItems: 'flex-start', 
-    },
-    form: {
-        flex: 1,//primeiro item
-        marginRight: 10,
-        padding: 20,
-        backgroundColor: '#F2F2F2',
-        borderRadius: 10,
-        shadowColor: '#000',
-        shadowOpacity: 0.1,
-        shadowOffset: { width: 0, height: 4 },
-        shadowRadius: 5,
-        paddingBottom: 20,
     },
     formContainer: {
         flex: 1,
@@ -178,6 +173,11 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         marginBottom: 20,
     },
+    subtitle: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginBottom: 10,
+    },
     input: {
         borderWidth: 1,
         borderColor: '#ccc',
@@ -185,37 +185,18 @@ const styles = StyleSheet.create({
         padding: 10,
         marginBottom: 10,
     },
-    button: {
-        backgroundColor: "#3498db", // Cor de fundo do botão
-        padding: 10,
-        borderRadius: 5,
-        alignItems: "center",
-        justifyContent: "center",
-      },
-      buttonText: {
-        color: "#fff",
-        fontSize: 16,
-        fontWeight: "bold",
-      },
-    /*subtitle: {
-        fontSize: 18,
-        fontWeight: 'bold',
-        marginBottom: 10,
-    },
-    
-    postItem: {
+    recordItem: {
         padding: 10,
         marginVertical: 5,
         backgroundColor: '#f8f8f8',
         borderRadius: 5,
     },
-    postText: {
+    recordText: {
         fontSize: 16,
         fontWeight: 'bold',
     },
-    postUrl: {
-        fontSize: 14,
-        color: '#007BFF',
-        marginBottom: 5,
-    },*/
 });
+
+
+
+
