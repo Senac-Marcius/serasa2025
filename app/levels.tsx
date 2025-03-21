688ec424e1890a82f1509b1ae26771222672c59f
aneliza-caleran <aneliza-caleran>
2025-03-20 22:34:34 -0300

    levelstsx

--- app/levels.tsx
+++ app/levels.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { View,Text, StyleSheet,FlatList, Button,TextInput} from 'react-native';
+import { View,Text, StyleSheet,FlatList, Button,TextInput, Touchable, TouchableOpacity} from 'react-native';
+import MyLevels from '../src/components/Mylevels';
 
 export default function categoryScreen(){
     const [req, setReq] = useState({
@@ -22,6 +23,9 @@ export default function categoryScreen(){
         }[]>([])
     
     function handleRegister (){
+
+
+        
         setleves([...leves, req])
         setReq({name:'',
             description:'',
@@ -33,8 +37,24 @@ export default function categoryScreen(){
         })
     }
 
+    function editLevels (id:number){
+        const notification = leves.find( l => l.id == id)
+        if(notification)
+            setReq(notification)
+
+    }
+
+    function deleteLevels (id:number){
+        
+
+    }
+
+
     return (
         <View>
+            < MyLevels style={styles.row}>
+                <text></text>
+            </MyLevels>
     {/* aqui é typerscrypt dentro do front */}
 
             <View style={styles.row}>
@@ -55,7 +75,6 @@ export default function categoryScreen(){
                         onChangeText={(text) => setReq({...req ,color: text})}
                     />
                         
-                    
                       
                    <Button title= 'Cadastrar' onPress= {handleRegister}/>
                                  
@@ -64,13 +83,34 @@ export default function categoryScreen(){
                     data={leves}
                     keyExtractor={(item) => item.id.toString()}
                     renderItem={({item}) =>(
-                        <view>
+                        <View>
                             <text>{item.name}</text>
-                        <text>{item.createAt}</text> 
-                        <text>{item.name}</text>
-                        <text>{item.color}</text>
-                        <text>{item.userId}</text>  
-                        </view>
+                            <text>{item.createAt}</text> 
+                            <text>{item.name}</text>
+                            <text>{item.color}</text>
+                            <text>{item.userId}</text>  
+
+                            <View style={styles.buttonsContainer}>
+                                <TouchableOpacity 
+                                style={styles.editButton}
+                                onPress={() => {editLevels(item.id)}}>
+
+                                    <Text style={styles.buttonText}>EDIT</Text>
+
+                                </TouchableOpacity>
+
+                                <TouchableOpacity 
+                                style={styles.delButton}
+                                onPress={() => {deleteLevels(item.id)}}>
+
+                                    <Text style={styles.buttonText}>DELETE</Text>
+
+                                </TouchableOpacity>
+                               
+                            </View>
+                        </View>
+
+                        
                     )}
 
                 />
@@ -139,4 +179,21 @@ const styles = StyleSheet.create({
         color: '#007BFF',
         marginBottom: 5,
     },
+    buttonText:{
+        color:'#000000',
+        fontWeight: 'bold'
+    },
+    buttonsContainer:{
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap:20,
+        alignContent:'space-around',
+    },
+    editButton:{
+
+    },
+    delButton:{
+
+    },
+
 });
