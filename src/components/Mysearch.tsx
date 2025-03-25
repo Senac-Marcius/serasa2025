a16529fe45ecaf5014f40bf5f006575101ba0e8b
Steffeens <marciosteffenspley@gmail.com>
2025-03-24 21:29:35 -0300

    Componente de Pesquisa

--- src/components/Mysearch.tsx
+++ src/components/Mysearch.tsx
@@ -1,21 +1,43 @@
 
-import React, {ReactNode} from 'react';
+import React from 'react';
 import {Text, View, ViewStyle} from "react-native";
+import { TextInput } from 'react-native-gesture-handler';
+import { IconButton } from 'react-native-paper';
 
 interface MySearchProps {
     style: ViewStyle;
-    children: ReactNode;
+   onKeyPress(): void; 
 }
 
-const MySearch: React.FC<MySearchProps> = ({children, style}) => {
+const MySearch: React.FC<MySearchProps> = ({onKeyPress, style}) => {
     return (
-        <View>
-            <Text> Ola Componente </Text>
-            {children}
-            <Text> Tchau Componente </Text>
+        <View style={[{padding: 10, flexDirection: 'row', alignItems: 'center' }, style]}>
+            <Text> </Text>
+            <IconButton 
+                icon="magnify" // Ícone de pesquisa do react-native-paper
+                size={20}
+                onPress={onKeyPress}
+                style={{ position: 'absolute', left: 10 }} // Colocando o ícone à esquerda da TextInput
+            />           
+            <TextInput 
+                 style={{
+                    flex: 1,
+                    paddingLeft: 30, // Espaço para o ícone dentro da barra de pesquisa
+                    paddingRight: 10,
+                    borderWidth: 1,
+                    borderRadius: 25,
+                    borderColor: '#ccc',
+                    height: 40,
+                    fontSize: 19,
+                }}
+                placeholder="Pesquisar..."
+                onKeyPress={(e) => onKeyPress()}
+            />
         </View>
     );
-}
+};
 
 export default MySearch
+
+
 
