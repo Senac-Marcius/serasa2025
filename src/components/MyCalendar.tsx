583b5c830f410388b487346c134d3586b50dbbe8
YisaVi <isabelevitorio78@gmail.com>
2025-03-25 20:59:09 -0300

    calendario

--- src/components/MyCalendar.tsx
+++ src/components/MyCalendar.tsx
@@ -1,14 +1,61 @@
 import { View } from 'native-base';
-import { Text, ViewStyle } from 'react-native';
-import React, {ReactNode}  from "react";
+import { Text, ViewStyle, ScrollView } from 'react-native';
+import React, {ReactNode, useState }  from "react";
+import { DatePickerModal } from 'react-native-paper-dates';
+import { useRouter } from 'expo-router';
 
-interface MyCalendarProp {
+export default function LoanScreen() {
+    const router = useRouter();
+
+    const [date, setDate] = useState<Date | undefined>(undefined);
+    const [open, setOpen] = useState(false);
+}
+
+/*interface MyCalendarProp { //teste aula
     style: ViewStyle;
     children: ReactNode;
-} 
- 
+} */
+interface CalendarDate {
+    year: number;
+    month: number;
+    day: number;
+  }
+  const [calendarDate, setCalendarDate] = useState<CalendarDate | null>(null); //tipo modificado para aceitar valores nulos
 
-const MyCalendar: React.FC <MyCalendarProp> = ({children, style}) => {
+// Tipagem para o modal de data
+interface MyCalendarProps {
+    date?: string;
+    setDate(date:String | undefined): void;
+}
+
+const MyCalendar : React.FC<MyCalendarProps> = ({ date, setDate }) => {
+    const [open, setOpen] = useState(false);
+
+return (
+    <ScrollView>
+    <View>
+
+        <DatePickerModal
+            locale="pt"
+            mode="single"
+            visible={open}
+            onDismiss={() => setOpen(false)}
+            date={date? new Date(date): new Date()}
+            onConfirm={(params) => {
+                setDate(params.date?.toString());
+                setOpen(false);
+            }}
+        />
+
+        {date && (
+            <Text>{`Data Selecionada: ${date}`}</Text> //está exibindo um texto em um componente React Native usando a tag <Text>
+        )}
+    </View>
+    </ScrollView>
+)};
+
+
+/*const MyCalendar: React.FC <MyCalendarProp> = ({children, style}) => { //teste aula
     return (
         <View style={style}>
         <Text>ola componente</Text>
@@ -16,11 +63,11 @@ const MyCalendar: React.FC <MyCalendarProp> = ({children, style}) => {
         </View>
     );
 }
-const MyCalendar2 = () => {
+const MyCalendar2 = () => { //teste aula
     return (
         <Text>ola componente2</Text>
         
     );
-}
+}*/
 
-export {MyCalendar, MyCalendar2}
+export {MyCalendar}
