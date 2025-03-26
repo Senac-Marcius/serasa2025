aeac57af5e59253542175211e86b1c7235d6c4a7
MarciusMcflay <jhorge.marcius@gmail.com>
2025-03-25 22:44:17 -0300

    aula

--- src/components/Mytest.tsx
+++ src/components/Mytest.tsx
@@ -1,27 +1,39 @@
 import React, {Children, ReactNode, useState}  from "react";
-import { Text, TouchableOpacity, View, ViewStyle, StyleSheet } from "react-native";
+import {  Text, TouchableOpacity, View, ViewStyle, StyleSheet } from "react-native";
 import { FlatList, TextInput } from "react-native-gesture-handler";
-import { Button, List } from "react-native-paper";
+import {Button, List } from "react-native-paper";
 
 interface MyTestProps {
-  style?: ViewStyle;
-  itens: string[];
-  onPress(item:string): void;
-  chendTheme(): void
+  label: string;
+  list: {key: any, option: string}[];
+  setLabel(item:string):void;
+  setKey?(key:any):void;
 }
 
-const MyTest: React.FC<MyTestProps> = ({style, itens, onPress}) => {
+const [visible, setVisible] = useState(false)
+
+const MyTest: React.FC<MyTestProps> = ({label, list, setLabel, setKey}) => {
   return (
-    <FlatList
-      style={style}
-      data={itens}
-      keyExtractor={(item, index) => index.toString()}
-      renderItem={({ item }) => (
-        <TouchableOpacity style={styles.item} onPress={() => onPress(item)}>
-          {item}
-        </TouchableOpacity>
-      )}
-     />
+    <View>
+      <Button onPress={ ()=> {setVisible(true)} }>{label}   </Button>
+      {
+        visible &&    
+        (<FlatList 
+            data={list}
+            keyExtractor={(item) => item.key}
+            renderItem={(item) => (
+              <TouchableOpacity onPress={()=>{
+                setLabel(item.item.option);
+                if(setKey){
+                  setKey(item.item.key)
+                }
+              }}>
+                <Text>{item.item.option}</Text>
+              </TouchableOpacity>
+            )}
+        />)
+      }
+    </View>
   );
 }
 
