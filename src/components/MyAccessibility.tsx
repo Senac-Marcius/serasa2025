1d22de14abf7045cc2a753af0d2306d8bf2850ea
guilherme jesus silva <gulhermejesus6@gmail.com>
2025-03-26 22:02:58 -0300

    icon

--- src/components/MyAccessibility.tsx
+++ src/components/MyAccessibility.tsx
@@ -40,20 +40,23 @@ const MyAccess: React.FC<MyAccessProps> = ({ children, style }) => {
   return (
     <View style={[styles.container, style]}>
       {children}
+      
+      {/* Botão de acessibilidade com ícone */}
       <TouchableOpacity
         style={styles.suporteButton}
         onPress={handleSuporte}
         accessible={true}
         accessibilityLabel="Botão de Suporte"
+        accessibilityHint="Clique para obter ajuda de acessibilidade"
         accessibilityRole="button"
       >
-        <Text style={styles.suporteText}>Accessibility</Text>
+        <MaterialIcons name="universal-access" size={30} color="white" />
+        <Text style={styles.suporteText}>Acessibilidade</Text>
       </TouchableOpacity>
     </View>
   );
 };
 
-<MaterialIcons name="universal-access" size={40} color="black" />
 
 const styles = StyleSheet.create({
   container: {
@@ -90,6 +93,7 @@ const styles = StyleSheet.create({
   suporteText: {
     color: 'white',
     fontSize: 12,
+    marginTop: 5,
   },
 });
 
