f642244965f6d9715b56e4514f5c10ec047246ec
vitorgianeis <vitorhgianeis@gmail.com>
2025-03-28 19:40:46 -0300

    atualizando

--- app/revenues.tsx
+++ app/revenues.tsx
@@ -9,8 +9,6 @@ import MyTopbar from '../src/components/mytopbar';
 import Myiten from '../src/components/myItenlist';
 import Mytext from '../src/components/Mytext';
 
-
-
 export default function RevenueScreen() {
   // Estado para o formulário
   const [req, setReq] = useState({
@@ -76,94 +74,76 @@ export default function RevenueScreen() {
     setRevenues(list); // Remove a receita da lista
   }
 
-
   return (
-    
-
-    
     <MyView>
-     
-
-
-       <Mytext style={{ fontSize: 24, fontWeight: 'bold', color: '#333', textAlign: 'center' }}>
-       cadastre as receitas
-       </Mytext>
-       
+      <Mytext style={{ fontSize: 24, fontWeight: 'bold', color: '#333', textAlign: 'center' }}>
+        cadastre as receitas
+      </Mytext>
 
       {/* Formulário */}
-      
       <View style={styles.row}>
         <View style={styles.form}>
-        <View style={styles.form}>
-  {/* Campo de Nome */}
-  <Myinput
-    value={req.name}
-    onChangeText={(text) => setReq({ ...req, name: text })}
-    iconName=''
-    placeholder='Digite o nome'
-    label='Nome'
-  />
-  
-  
-  
-  {/* Campo de Descrição */}
-  <MyTextArea 
-    value={req.description} 
-    onChangeText={(text) => setReq({...req, description: text})}  
-    iconName='' 
-    placeholder='Digite a descrição'
-    label='Descrição'
-  />
+          <View style={styles.form}>
+            {/* Campo de Nome */}
+            <Myinput
+              value={req.name}
+              onChangeText={(text) => setReq({ ...req, name: text })}
+              iconName=''
+              placeholder='Digite o nome'
+              label='Nome'
+            />
+            
+            {/* Campo de Descrição */}
+            <MyTextArea 
+              value={req.description} 
+              onChangeText={(text) => setReq({...req, description: text})}  
+              iconName='' 
+              placeholder='Digite a descrição'
+              label='Descrição'
+            />
 
-  
+            {/* Campo de URL */}
+            <Myinput
+              value={req.url}
+              onChangeText={(text) => setReq({ ...req, url: text })}
+              iconName='link'
+              placeholder='Digite a URL'
+              label='URL'
+            />
 
-  {/* Campo de URL */}
-  <Myinput
-  value={req.url}
-  onChangeText={(text) => setReq({ ...req, url: text })}
-  iconName='link'
-  placeholder='Digite a URL'
-  label='URL'
- 
-/>
+            {/* Campo de Valor */}
+            <Myinput
+              value={req.value}
+              onChangeText={(text) => setReq({ ...req, value: text })}
+              iconName=''
+              placeholder='Digite o valor'
+              label='Valor'
+            />
 
-  {/* Campo de Valor */}
-  <Myinput
-    value={req.value}
-    onChangeText={(text) => setReq({ ...req, value: text })}
-    iconName=''
-    placeholder='Digite o valor'
-    label='Valor'
-    
-  />
+            {/* Campo de Status da Bolsa */}
+            <Myinput
+              value={req.scholarshipStatus}
+              onChangeText={(text) => setReq({ ...req, scholarshipStatus: text })}
+              iconName=''
+              placeholder='Status da bolsa'
+              label='Status Bolsa'
+            />
 
-  {/* Campo de Status da Bolsa */}
-  <Myinput
-    value={req.scholarshipStatus}
-    onChangeText={(text) => setReq({ ...req, scholarshipStatus: text })}
-    iconName=''
-    placeholder='Status da bolsa'
-    label='Status Bolsa'
-  />
-
-  {/* Campo de Desconto */}
-  <Myinput
-    value={req.discountPercentage}
-    onChangeText={(text) => setReq({ ...req, discountPercentage: text })}
-    iconName='percent'
-    placeholder='Porcentagem de desconto'
-    label='Desconto'
-    
-  />
-
-  <Button title="Cadastrar" onPress={handleRegister} />
-</View>
-
-          
+            {/* Campo de Desconto */}
+            <Myinput
+              value={req.discountPercentage}
+              onChangeText={(text) => setReq({ ...req, discountPercentage: text })}
+              iconName='percent'
+              placeholder='Porcentagem de desconto'
+              label='Desconto'
+            />
+            <MyButton title= "cadastrar" onPress={handleRegister}/>
+            
+          </View>
         </View>
 
         {/* Lista de Receitas */}
-        <MyList        /* Lista componente*/
+        <MyList
           data={revenues}
           keyItem={(item) => item.id.toString()}
           renderItem={({ item }) => (
@@ -172,15 +152,14 @@ export default function RevenueScreen() {
               onEdit={()=> {editRevenue(item.id)}}
               onDel={()=> {delRevenue(item.id)}}
             >
-               <Mytext style={styles.revenueText}>Descrição: {item.description}</Mytext>
-                <Mytext style={styles.revenueText}>Nome: {item.name}</Mytext>
-               <Mytext style={styles.revenueText}>ID do Usuário: {item.userId}</Mytext>
-               <Mytext style={styles.revenueText}>Valor: {item.value}</Mytext>
-               <Mytext style={styles.revenueText}>Status da Bolsa: {item.scholarshipStatus}</Mytext>
-               <Mytext style={styles.revenueText}>Desconto: {item.discountPercentage}%</Mytext>
-               <Mytext style={styles.revenueText}>Data: {item.createAt}</Mytext>
-               <Mydownload style={styles.revenueText} url={item.url}/>
-
+              <Mytext style={styles.revenueText}>Descrição: {item.description}</Mytext>
+              <Mytext style={styles.revenueText}>Nome: {item.name}</Mytext>
+              <Mytext style={styles.revenueText}>ID do Usuário: {item.userId}</Mytext>
+              <Mytext style={styles.revenueText}>Valor: {item.value}</Mytext>
+              <Mytext style={styles.revenueText}>Status da Bolsa: {item.scholarshipStatus}</Mytext>
+              <Mytext style={styles.revenueText}>Desconto: {item.discountPercentage}%</Mytext>
+              <Mytext style={styles.revenueText}>Data: {item.createAt}</Mytext>
+              <Mydownload style={styles.revenueText} url={item.url}/>
             </Myiten>
           )}
         />
@@ -190,11 +169,6 @@ export default function RevenueScreen() {
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-    backgroundColor: '#f5f5f5', // Fundo da tela
-  },
   row: {
     flexDirection: 'row',
     justifyContent: 'space-between',
@@ -204,69 +178,30 @@ const styles = StyleSheet.create({
     flex: 1,
     marginRight: 10,
     padding: 20,
-    backgroundColor: '#fff', // Fundo branco
+    backgroundColor: '#D3D3D3',
     borderRadius: 10,
     shadowColor: '#000',
     shadowOpacity: 0.1,
     shadowOffset: { width: 0, height: 4 },
     shadowRadius: 5,
-    elevation: 3, // Sombra no Android
-  },
-  input: {
-    height: 40,
-    borderColor: '#ccc',
-    borderWidth: 1,
-    borderRadius: 5,
-    marginBottom: 15,
-    paddingHorizontal: 10,
-    backgroundColor: '#f9f9f9', // Fundo do input
+    elevation: 3,
   },
   revenueStyle: {
     flex: 1,
     marginRight: 10,
     padding: 15,
-    backgroundColor: '#fff', // Fundo branco
+    backgroundColor: '#D3D3D3',
     borderRadius: 10,
     shadowColor: '#000',
     shadowOpacity: 0.1,
     shadowOffset: { width: 0, height: 4 },
     shadowRadius: 5,
-    elevation: 3, // Sombra no Android
-    marginBottom: 10, // Espaçamento entre os itens
+    elevation: 3,
+    marginBottom: 10,
   },
   revenueText: {
     fontSize: 14,
-    color: '#000000', // Texto preto
-    marginBottom: 5, // Espaçamento entre os textos
-  },
-  buttonsContanier: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 20,
-    justifyContent: 'space-around',
-  },
-  editButton: {
-    backgroundColor: '#FFFF00', // Cor de fundo AMARELO
-    padding: 10,
-    
-  },
-  deleteButton: {
-    backgroundColor: '#f44336', // Cor de fundo vermelho
-    padding: 10,
-  
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  buttonText: {
-    color: '#000000', // Texto preto
-    fontWeight: 'bold',
-  },
-
-  downloadButton:{
-    backgroundColor: '#A020F0', 
-    padding: 10,
-    borderRadius: 5,
-    alignItems: 'center',
-    justifyContent: 'center',
+    color: '#000000',
+    marginBottom: 5,
   }
 });
