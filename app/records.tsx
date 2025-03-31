4253361557d1dcbea51cea330d5ce78fd064b47e
gabi02447 <gabriela.pramparo247@gmail.com>
2025-03-31 20:27:05 -0300

    melhorando a cada dia

--- app/records.tsx
+++ app/records.tsx
@@ -1,32 +1,33 @@
-import React, {useState} from 'react';
-import {View, Text, StyleSheet, TextInput, Button, FlatList, TouchableOpacity} from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TextInput, Button, FlatList, TouchableOpacity } from 'react-native';
 import MyView from '../src/components/MyView';
 import MyButton from '../src/components/Mybuttons';
 import { IconButton } from 'react-native-paper';
-import { Myinput } from '../src/components/Myinputs';
+import { Myinput, MyCheck, MyTextArea } from '../src/components/Myinputs';
 
-export default function RecordScreen(){
-{/*Aqui é typescript COMENTÁRIO dentro do front */}
 
-    const [req, setReq] = useState ({
-        id:0,
-        name:'',
-        description:'',
-        sick:'',
-        health:'',
-        allergy:'',
-        medication:'',
-        userId:0,
+export default function RecordScreen() {
+    {/*Aqui é typescript COMENTÁRIO dentro do front */ }
+
+    const [req, setReq] = useState({
+        id: 0,
+        name: '',
+        description: '',
+        sick: '',
+        health: '',
+        allergy: '',
+        medication: '',
+        userId: 0,
         createAt: new Date().toISOString(),
     });
 
     const [records, setRecords] = useState<{
         id: number
         name: string,
-        description: string, 
-        sick: string, 
-        health: string, 
-        allergy: string, 
+        description: string,
+        sick: string,
+        health: string,
+        allergy: string,
         medication: string,
         userId: number,
         createAt: string,
@@ -34,37 +35,37 @@ export default function RecordScreen(){
     }[]>([]);
 
     function handleRegister() {
-        if(req.id ==-1){
-            const newId = records.length ? records[records.length -1].id +1 : 0;
-            const newRecord = {...req, id: newId};
+        if (req.id == -1) {
+            const newId = records.length ? records[records.length - 1].id + 1 : 0;
+            const newRecord = { ...req, id: newId };
 
             setRecords([...records, req]);
-        }else{
-            setRecords(records.map(r =>(r.id == req.id ? req : r))); 
+        } else {
+            setRecords(records.map(r => (r.id == req.id ? req : r)));
         }
-        
+
         setReq({
             id: -1,
-            name:'',
-            description:'',
-            sick:'',
-            health:'',
-            allergy:'',
-            medication:'',
-            userId:0,
+            name: '',
+            description: '',
+            sick: '',
+            health: '',
+            allergy: '',
+            medication: '',
+            userId: 0,
             createAt: new Date().toISOString(),
         })
     }
 
-    function editRecord(id:number){ 
-        const record = records.find (r => r.id == id)
-        if(record)
+    function editRecord(id: number) {
+        const record = records.find(r => r.id == id)
+        if (record)
             setReq(record)
     }
 
-    function delRecord(id:number){
-        const list = records.filter (r => r.id != id)
-        if(list)
+    function delRecord(id: number) {
+        const list = records.filter(r => r.id != id)
+        if (list)
             setRecords(list)
     }
 
@@ -72,74 +73,105 @@ export default function RecordScreen(){
 
         <MyView>
 
-        <Text> Tela dos Registros </Text>
 
             <View style={styles.row}>
-            
+
 
                 <View style={styles.form}>
 
 
-                    <TextInput
-                        placeholder="Nome do Aluno:"
+
+                    <Myinput
+                        placeholder="Digite"
                         value={req.name}
-                        onChangeText={(text) => setReq({...req, name: text }) }
+                        onChangeText={(text) => setReq({ ...req, name: text })}
+                        label='Nome do Aluno:'
+                        iconName='person'
                     />
 
-                    <TextInput
-                        placeholder="Descrição:"
+                    < MyTextArea
                         value={req.description}
-                        onChangeText={(text) => setReq({...req, description: text }) }
+                        onChangeText={(text) => setReq({ ...req, description: text })}
+                        placeholder='Descrição'
+                        label='Descreva a situação:'
+                        iconName='book'
                     />
 
-                    <TextInput
-                        placeholder="Doença:"
+                    < Myinput
                         value={req.sick}
-                        onChangeText={(text) => setReq({...req, sick: text }) }
+                        onChangeText={(text) => setReq({ ...req, sick: text })}
+                        placeholder='Doença'
+                        label='Coloque a doença:'
+                        iconName='sick'
                     />
 
-                    <TextInput
-                        placeholder="Saúde:"
+                    <MyTextArea
                         value={req.health}
-                        onChangeText={(text) => setReq({...req, health: text }) }
+                        onChangeText={(text) => setReq({ ...req, health: text })}
+                        placeholder="Saúde"
+                        label='Descreva o estado da saúde do aluno:'
+                        iconName='book'
                     />
 
-                    <TextInput
-                        placeholder="Alergia:"
-                        value={req.allergy}
-                        onChangeText={(text) => setReq({...req, allergy: text }) }
+                    < Myinput
+                        value={req.sick}
+                        onChangeText={(text) => setReq({ ...req, allergy: text })}
+                        placeholder='Alergia'
+                        label='Coloque as alergias do aluno:'
+                        iconName='biotech'
                     />
 
-                    <TextInput
-                        placeholder="Medicações:"
-                        value={req.medication}
-                        onChangeText={(text) => setReq({...req, medication: text }) }
+                    < Myinput
+                        value={req.sick}
+                        onChangeText={(text) => setReq({ ...req, medication: text })}
+                        placeholder='Medicação'
+                        label='Coloque as medicações de uso do aluno:'
+                        iconName='medication'
                     />
 
-                    <Button title="Cadastrar:" onPress={ handleRegister } />
+                    <MyButton
+                        title="CADASTRAR"
+                        onPress={handleRegister}
+                        button_type="round"
+                        style={styles.button_round}
+                    />
 
                 </View>
 
 
-                <FlatList
+                <FlatList // MyList
                     data={records}
-                    keyExtractor={( item ) => item.id.toString()} 
+                    keyExtractor={(item) => item.id.toString()}
                     renderItem={({ item }) => (
-                        <View style={styles.recordsItem}>
-                            <Text>Nome: {item.name}</Text>
-                            <Text>Descrição: {item.description}</Text>
-                            <Text>Doença: {item.sick}</Text>
-                            <Text>Saúde: {item.health}</Text>
-                            <Text>Alergias: {item.allergy}</Text>
-                            <Text>Medicações: {item.medication}</Text>
-                            <Text>Usuário Id: {item.userId}</Text>
+                        <View style={styles.recordsItem}  /*MyItem */> 
+                            <Text style={styles.itemText}>Nome: {item.name}</Text>
+                            <Text style={styles.itemText}>Descrição: {item.description}</Text>
+                            <Text style={styles.itemText}>Doença: {item.sick}</Text>
+                            <Text style={styles.itemText}>Saúde: {item.health}</Text>
+                            <Text style={styles.itemText}>Alergias: {item.allergy}</Text>
+                            <Text style={styles.itemText}>Medicações: {item.medication}</Text>
+                            <Text style={styles.itemText}>Usuário Id: {item.userId}</Text>
 
-                            <View style={styles.buttonsContainer}>
-                                <TouchableOpacity onPress={( ) => {editRecord(item.id)}}>EDIT</TouchableOpacity>
-                                <TouchableOpacity onPress={ ( ) => {delRecord(item.id)}}>DELETE</TouchableOpacity>
+                            <View style={styles.button_round}>
+
+                                <MyButton
+                                    title="EXCLUIR"
+                                    onPress={() => { delRecord(item.id) }}
+                                    button_type="round"
+                                    style={styles.button_round}
+                                />
+
+                                <MyButton
+                                    title=" EDITAR"
+                                    onPress={() => { delRecord(item.id) }}
+                                    button_type="round"
+                                    style={styles.button_round}
+                                />
+
+
                             </View>
                         </View>
-                    )}                                                                        
+                    )}
                 />
             </View>
         </MyView>
@@ -148,15 +180,16 @@ export default function RecordScreen(){
 
 const styles = StyleSheet.create({
     row: {
-        flexDirection: 'row', 
-        justifyContent: 'space-between', 
-        alignItems: 'flex-start', 
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'flex-start',
     },
+
     form: {
         flex: 1,
         marginRight: 20,
         marginLeft: 20,
-        padding: 20,
+        padding: 30,
         backgroundColor: '#F2F2F2',
         borderRadius: 10,
         shadowColor: '#000',
@@ -164,7 +197,8 @@ const styles = StyleSheet.create({
         shadowOffset: { width: 0, height: 4 },
         shadowRadius: 5,
     },
-    recordsItem:{
+
+    recordsItem: {
         flex: 1,
         marginRight: 20,
         marginLeft: 20,
@@ -177,12 +211,23 @@ const styles = StyleSheet.create({
         shadowRadius: 5,
     },
 
-    buttonsContainer:{
-        flexDirection: 'row',
-        alignItems: 'center',
-        gap: 20,
-        alignContent: 'space-around',
-  
+    button_round: {
+        borderRadius: 20,
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "row",
+
     },
 
+    itemText: {
+        color: 'black',
+        fontSize: 16,
+        marginBottom: 5,
+    },
+
+    buttonText: {
+        color: '#fff',
+        fontSize: 16,
+        fontWeight: 'bold',
+    }
 })
