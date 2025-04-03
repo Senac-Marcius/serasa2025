8845fd352a3f6ad0569d43b70b16cc3db1fb2759
DanFerrari <danieldasilvaferrari@gmail.com>
2025-04-02 21:57:26 -0300

    adicionado controlador de estudantes com funções básicas de CRUD

---
+++ src/controllers/students.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { supabase } from '../utils/supabase'
+
+
+interface iStudent {
+    id: number;
+    name: string;
+    password: string;
+    email: string;
+    createdAt: string;
+}
+
+const [student, setStunt] = useState<iStudent[]>([]);
+
+async function setStudent() {
+  const { data, error } = await supabase
+    .from("students")
+    .insert([{ some_column: "someValue", other_column: "otherValue" }])
+    .select();
+}
+
+async function Delete() {}
+
+async function Update() {}
+
+async function Select() {}
