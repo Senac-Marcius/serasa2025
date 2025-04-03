75f7c11a4cbb78e890feec777cc00c73be2f42dc
cecilic <cecilialima0101@gmail.com>
2025-04-03 19:28:00 -0300

    comit

---
+++ src/controllers/budgets.tsx
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import { supabase } from '../utils/supabase';
+ 
+interface iBudgets{
+    id: number;
+    name: string;
+    url: string;
+    createAt: string;
+    userId: number;
+    value: string;
+    startDate: string;
+    endDate: string;
+}
+ 
+ 
+// Estado para a lista de receitas
+const [budgets, setBudgets] = useState<iBudgets[]>([]);
+ 
+ 
+ 
+async function  setBudget(budget:iBudgets ){
+    // aqui vem os tratamnetos de regex ou do modelo de negocio antes de inserir
+       // Regex definitions
+
+    const idRegex = /^\d+$/;
+    const nameRegex =/^[a-zA-ZÀ-ÿ\u00f1\u00d1\s-]{2,100}$/;
+    const urlRegex =/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/;
+    const valueRegex = /^\d+(\.\d{1,2})?$/;
+    const startDateRgex = /^\d{4}-\d{2}-\d{2}$/;
+    const endDateRegex = /^\d{4}-\d{2}-\d{2}$/;            
+      
+    // Validation checks
+    if (!idRegex.test(String(budget.id))) {
+        return "Campo id deve conter apenas números";
+    }
+ 
+    if (!nameRegex.test(budget.name)) {
+        return "Campo name deve conter apenas letras, espaços ou hífens (2-100 caracteres)";
+    }
+ 
+    if (!urlRegex.test(budget.url)) {
+        return "Campo url deve conter o seguinte formato https://dominio";
+    }
+ 
+    if (!idRegex.test(String(budget.userId))) {
+        return "Campo userId deve conter apenas números";
+    }
+ 
+    if (!valueRegex.test(budget.value)) {
+        return "Campo value deve ser um valor monetário (ex: '100', 'R$ 50,00')";
+    }
+    if (!startDateRgex.test(budget.createAt)) {
+        return "Campo createAt deve conter o seguinte formato YYYY-MM-DD";
+    }
+    if (!endDateRegex.test(budget.createAt)) {
+        return "Campo createAt deve conter o seguinte formato YYYY-MM-DD";
+    }
+ 
+ 
+    const { data, error } = await supabase
+    .from('budgets')
+    .insert([
+        budget
+    ])
+    .select()
+ 
+   
+   
+    if (error){
+ // aqui vem os tratamentos da variavel error
+ 
+ 
+        return[]
+    }
+ 
+    return data
+}
+ 
+export {setBudgets, budgets, setBudget}
