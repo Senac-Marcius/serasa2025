e4fa3b1ebf5bd50a5034175b93dff99f9d26b3d9
karolinascsp <karolinasc.sp@gmail.com>
2025-04-02 22:28:15 -0300

    criando o controler e regex

--- src/controllers/timeline.tsx
+++ src/controllers/timeline.tsx
@@ -20,6 +20,24 @@ const [timelines, setTimelines] = useState<iTimeline[]>([]);
 
 async function setTimeline(timeline:iTimeline){
     //aqui vem os tratamentos do regex ou do modelo de negocio antes de inserir
+    
+    const urlRegex = /^(https?:\/\/)?([a-z0-9]+(\.[a-z0-9]+)+)(\/[a-z0-9#?&%=]*)?$/i;
+    const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
+    const timeRegex = /^([01]?[0-9]|2[0-3]):([0-5]?[0-9])$/
+
+
+    if (!urlRegex.test(req.url)) {
+        return "Campo url deve conter o formato https:siteorganizador";
+    }
+
+    if (!dateRegex.test(req.date)) {
+        return "Campo deve conter o formato yyyy-mm-dd";
+    }
+
+    if (!timeRegex.test(req.beginning)) {
+        return "Campo deve conter o formato 13:00";
+    }
+
 const { data, error } = await supabase
   .from('timeline')
   .insert([
