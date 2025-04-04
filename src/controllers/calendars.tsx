5314649fcd654818f1870de31f17374866aa6e61
SthefaneA <sthegarzon85@gmail.com>
2025-04-04 20:25:07 -0300

    supa

--- src/controllers/calendars.tsx
+++ src/controllers/calendars.tsx
@@ -1,11 +1,36 @@
-import React, {useState} from 'react';    
-    
-    const [req,setReq] = useState({
-        studentname: '',
-        course: '',
-        registrationdate: '',
-        period: '',
-        id: -1,
-        creadAt: new Date().toISOString(),
+import React, { useState } from 'react';
+import { supabase } from '../utils/supabase';
 
-});
+//iCalendar
+interface iCalendar {
+    studentname: string;
+    course: string;
+    registrationdate: string;
+    period: string;
+    id: number;
+    created_at: string;
+}
+
+const [calendars, setCalendars] = useState<iCalendar[]>([]);
+
+// adiciona um calendário e trata os erros
+async function setCalendarsData(calendar: iCalendar) {
+    const { data, error } = await supabase.from('calendars')
+        .insert([calendar])
+        .select();
+
+    // Tratamento de erros
+    if (error) {
+        alert('Preencha todos os dados!');
+        return null; // Retorna null em caso de erro
+    }
+
+    // Atualiza o estado com os novos dados do calendário
+    setCalendars((prevCalendars) => [...prevCalendars, ...data]);
+
+    return data;
+}
+
+export { setCalendars };
+
+
