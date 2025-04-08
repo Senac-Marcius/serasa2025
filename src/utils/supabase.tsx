7fb3ae12d56b81605f3f2e92add0cb631e09f6c6
minu1123 <miszaelnunescosta@Gmail.com>
2025-04-08 19:27:23 -0300

    supa correto

--- src/utils/supabase.tsx
+++ src/utils/supabase.tsx
@@ -1,17 +1,9 @@
 
 import { createClient } from "@supabase/supabase-js";
 
-<<<<<<< HEAD
 const supabaseUrl = "https://fcjbnmhbjolybbkervgg.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZjamJubWhiam9seWJia2VydmdnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI5MzcyNTQsImV4cCI6MjA1ODUxMzI1NH0.mFa5W8ixlKQtaNm_EdGFg3IuooF95Xcn-ArPx_vX4mI";
-<<<<<<< HEAD
-=======
-const supabaseUrl = 'https://fcjbnmhbjolybbkervgg.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZjamJubWhiam9seWJia2VydmdnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI5MzcyNTQsImV4cCI6MjA1ODUxMzI1NH0.mFa5W8ixlKQtaNm_EdGFg3IuooF95Xcn-ArPx_vX4mI';
->>>>>>> 5ee778693a7602e4524e2877f0eb403d1435ec62
-=======
-  
->>>>>>> dev_sthefane
+
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
         
