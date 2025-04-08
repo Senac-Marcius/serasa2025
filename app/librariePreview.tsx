e01db0a92ca250f8d7f31a29c447d97290a3c3fb
Yasmim <you@example.com>
2025-04-08 19:59:05 -0300

    SOCORRO

--- app/librariePreview.tsx
+++ app/librariePreview.tsx
@@ -3,6 +3,7 @@ import { useRouter } from 'expo-router';
 import { View, StyleSheet, TextInput, ScrollView, ViewStyle, FlatList, } from 'react-native';
 import MyText from '../src/components/MyText';
 import MyList from '../src/components/MyList';
+import MyItem from '../src/components/MyItem';
 import MyButton from '../src/components/MyButtons';
 import {iItem, setItem} from '../src/controllers/librarie';
 
@@ -63,50 +64,41 @@ export default function PreviewScreen() {
                 data={items}
                 keyItem={(item) => item.id.toString()}
                 renderItem={({ item }) => (
-                    <View style={styles.card}>
-                        <image />
-                        <MyText style={styles.cardText}>Tipologia: {item.typology}</MyText>
-                        <MyText style={styles.cardText}>Título: {item.title}</MyText>
-                        <MyText style={styles.cardText}>Subtítulo: {item.subtitle}</MyText>
-                        <MyText style={styles.cardText}>Responsáveis: {item.responsible}</MyText>
-                        <MyText style={styles.cardText}>Tradução: {item.translation}</MyText>
-                        <MyText style={styles.cardText}>Idioma: {item.language}</MyText>
-                        <MyText style={styles.cardText}>Ano: {item.year}</MyText>
-                        <MyText style={styles.cardText}>Edição: {item.edition}</MyText>
-                        <MyText style={styles.cardText}>Editora: {item.publisher}</MyText>
-                        <MyText style={styles.cardText}>Local: {item.location}</MyText>
-                        <MyText style={styles.cardText}>Número de Páginas: {item.number_pages}</MyText>
-                        <MyText style={styles.cardText}>Série: {item.serie}</MyText>
-                        <MyText style={styles.cardText}>Volume: {item.volume}</MyText>
-                        <MyText style={styles.cardText}>Formato: {item.format}</MyText>
-                        <MyText style={styles.cardText}>ISBN: {item.isbn}</MyText>
-                        <MyText style={styles.cardText}>ISSN: {item.issn}</MyText>
-                        <MyText style={styles.cardText}>CDD: {item.cdd}</MyText>
-                        <MyText style={styles.cardText}>Número de Chamada: {item.call_number}</MyText>
-                        <MyText style={styles.cardText}>Assunto: {item.subject}</MyText>
-                        <MyText style={styles.cardText}>Palavras-chave: {item.keywords}</MyText>
-                        <MyText style={styles.cardText}>Resumo: {item.summary}</MyText>
-                        <MyText style={styles.cardText}>Notas: {item.notes}</MyText>
-                        <MyText style={styles.cardText}>Número de Exemplares: {item.number_copies}</MyText>
-                        <MyText style={styles.cardText}>Status: {item.status}</MyText>
-                        <MyText style={styles.cardText}>URL: {item.url}</MyText>
-                        <MyText style={styles.cardText}>Arquivo: {item.file}</MyText>
-                        <MyText style={styles.cardText}>Tipo de Empréstimo: {item.type_loan}</MyText> 
-                        <View>
-                            <MyButton 
-                                title="EDITAR"
-                                onPress={ editItem }
-                                button_type="capsule"
-                                icon=""
-                            />
-                            <MyButton 
-                                title="DELETAR"
-                                onPress={ deleteItem }
-                                button_type="capsule"
-                                icon=""
-                            />
-                        </View> 
-                    </View>
+                    <MyItem
+                        onDel={()=>{deleteItem(item.id)}}
+                        onEdit={()=>{editItem(item.id)}}
+                    >
+                        <View style={styles.card}>
+                            <image />
+                            <MyText style={styles.cardText}>Tipologia: {item.typology}</MyText>
+                            <MyText style={styles.cardText}>Título: {item.title}</MyText>
+                            <MyText style={styles.cardText}>Subtítulo: {item.subtitle}</MyText>
+                            <MyText style={styles.cardText}>Responsáveis: {item.responsible}</MyText>
+                            <MyText style={styles.cardText}>Tradução: {item.translation}</MyText>
+                            <MyText style={styles.cardText}>Idioma: {item.language}</MyText>
+                            <MyText style={styles.cardText}>Ano: {item.year}</MyText>
+                            <MyText style={styles.cardText}>Edição: {item.edition}</MyText>
+                            <MyText style={styles.cardText}>Editora: {item.publisher}</MyText>
+                            <MyText style={styles.cardText}>Local: {item.location}</MyText>
+                            <MyText style={styles.cardText}>Número de Páginas: {item.number_pages}</MyText>
+                            <MyText style={styles.cardText}>Série: {item.serie}</MyText>
+                            <MyText style={styles.cardText}>Volume: {item.volume}</MyText>
+                            <MyText style={styles.cardText}>Formato: {item.format}</MyText>
+                            <MyText style={styles.cardText}>ISBN: {item.isbn}</MyText>
+                            <MyText style={styles.cardText}>ISSN: {item.issn}</MyText>
+                            <MyText style={styles.cardText}>CDD: {item.cdd}</MyText>
+                            <MyText style={styles.cardText}>Número de Chamada: {item.call_number}</MyText>
+                            <MyText style={styles.cardText}>Assunto: {item.subject}</MyText>
+                            <MyText style={styles.cardText}>Palavras-chave: {item.keywords}</MyText>
+                            <MyText style={styles.cardText}>Resumo: {item.summary}</MyText>
+                            <MyText style={styles.cardText}>Notas: {item.notes}</MyText>
+                            <MyText style={styles.cardText}>Número de Exemplares: {item.number_copies}</MyText>
+                            <MyText style={styles.cardText}>Status: {item.status}</MyText>
+                            <MyText style={styles.cardText}>URL: {item.url}</MyText>
+                            <MyText style={styles.cardText}>Arquivo: {item.file}</MyText>
+                            <MyText style={styles.cardText}>Tipo de Empréstimo: {item.type_loan}</MyText>
+                        </View>
+                    </MyItem>   
                 )}      
             />
             <MyButton 
