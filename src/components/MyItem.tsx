a8fc006ddb9685f52bfae5553f924dfeb790f442
PedroSouto543 <pedrospsouto@gmail.com>
2025-04-09 22:11:23 -0300

    colocando card de novo

--- src/components/MyItem.tsx
+++ src/components/MyItem.tsx
@@ -56,6 +56,19 @@ export  {MyItem, MyCorrelated}
 
 
 const styles = StyleSheet.create({
+  card: {
+    backgroundColor: "#f8f9fa",
+    padding: 16,
+    marginBottom: 8,
+    marginHorizontal: 12,
+    borderRadius: 8,
+    shadowColor: "#000",
+    shadowOpacity: 0.1,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 4,
+    elevation: 3, 
+   
+  },
   row : {
     flexDirection: 'row',
     justifyContent: 'space-between',
