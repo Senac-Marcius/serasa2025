6a1488ab6baf82702cf84ceadd14ca9b02866e25
gabi02447 <gabriela.pramparo247@gmail.com>
2025-04-09 21:41:09 -0300

    só para salvar

--- src/utils/supabase.tsx
+++ src/utils/supabase.tsx
@@ -1,13 +1,8 @@
 
 import { createClient } from "@supabase/supabase-js";
 
-<<<<<<< HEAD
 const supabaseUrl = 'https://fcjbnmhbjolybbkervgg.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZjamJubWhiam9seWJia2VydmdnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI5MzcyNTQsImV4cCI6MjA1ODUxMzI1NH0.mFa5W8ixlKQtaNm_EdGFg3IuooF95Xcn-ArPx_vX4mI';
-=======
-const supabaseUrl = "https://fcjbnmhbjolybbkervgg.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZjamJubWhiam9seWJia2VydmdnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI5MzcyNTQsImV4cCI6MjA1ODUxMzI1NH0.mFa5W8ixlKQtaNm_EdGFg3IuooF95Xcn-ArPx_vX4mI";
->>>>>>> afbe3a794b8fe1ddac1030579ce0c0bfec310667
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
         
