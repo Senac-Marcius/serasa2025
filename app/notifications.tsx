d89e3152ff08925ebd359e874f99e794fc0439cb
iris1518 <neuslinda12@gmail.com>
2025-04-10 20:09:47 -0300

    .

--- app/notifications.tsx
+++ app/notifications.tsx
@@ -32,7 +32,7 @@ export default function NotificationScreen(){
                 setNotifications(retorno.data);
             }
         }
-            getTodos();s
+            getTodos();
     },[])
     
     
