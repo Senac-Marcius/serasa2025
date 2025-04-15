2481e6f52b07fe34713d2987fe30aad8bbde5b5d
Onurbmastergit <brunoataide2002@gmail.com>
2025-04-14 21:39:12 -0300

    employees 1-1

--- src/utils/supabase.tsx
+++ src/utils/supabase.tsx
@@ -1,10 +1,6 @@
 
 import { createClient } from "@supabase/supabase-js";
 
-<<<<<<< HEAD
-
-=======
->>>>>>> ctr_librarie
 const supabaseUrl = "https://fcjbnmhbjolybbkervgg.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZjamJubWhiam9seWJia2VydmdnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI5MzcyNTQsImV4cCI6MjA1ODUxMzI1NH0.mFa5W8ixlKQtaNm_EdGFg3IuooF95Xcn-ArPx_vX4mI";
 
