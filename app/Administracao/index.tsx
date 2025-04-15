022f7f58ecab733f66a3c92032c73a0674380d74
MathCarvalh0 <matheus.carvalho.dechechi@hotmail.com>
2025-04-15 19:34:45 -0300

    index

---
+++ app/Administracao/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View } from 'react-native';
+import { useState } from 'react';
+import  MyTheme from '../../src/components/MyTheme'
+import  MyView from '../../src/components/MyView'
+import MyLogin from '../../src/components/MyLogin';
+import MyText from '../../src/components/MyText';
+import { useRouter } from 'expo-router';
+
+export default function HomeScreen() {
+    const router = useRouter();
+    const [email, setEmail] = useState('')
+    const [pass, setPass] = useState('')
+
+    return (
+        <MyView router={router} style={{ flex: 1 }}>
+            <MyTheme chendTheme={()=>{}} fontSize={()=>{}}/>
+
+            {/* Conteúdo da Página */}
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <MyLogin email={email} pass={pass} changeEmail={setEmail} changepass={setPass}>
+
+                <MyText>Bem vind@ </MyText>
+
+            </MyLogin>
+            </View>
+        </MyView>
+    );  
+} 
