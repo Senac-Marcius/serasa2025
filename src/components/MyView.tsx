18ccdb380b64305fabe894e6b5e7259aac4b9505
PedroSouto543 <pedrospsouto@gmail.com>
2025-04-16 20:49:45 -0300

    pusahndo outras brands

--- src/components/MyView.tsx
+++ src/components/MyView.tsx
@@ -53,6 +53,15 @@ const styles = StyleSheet.create({
     paddingHorizontal: 16,
     paddingBottom: 80,
   },
+  suporteButton: {
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
+    width: 60,
+    height: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   
   
 });
