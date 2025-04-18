f87fdb825ef9c0620f4432b4dadf7a468a512177
aneliza-caleran <aneliza-caleran>
2025-04-17 21:35:17 -0300

    união das dev

--- app/apoio/levels.tsx
+++ app/apoio/levels.tsx
@@ -23,6 +23,7 @@ export default function LevelsScreen() {
     description: '',
     color: '',
     created_at: new Date().toISOString(),
+
   });
 
   useEffect(() => {
@@ -31,6 +32,7 @@ export default function LevelsScreen() {
         const retorno = await getLevels({})
         if (retorno.status && retorno.data && retorno.data.length > 0 ) {
             setLevels(retorno.data); 
+
         }
 
       }
@@ -58,11 +60,13 @@ export default function LevelsScreen() {
         color: '',
         created_at:  new Date().toISOString(),
      });
+
   }
 
 async function deleteLevels(id: number) {
     setLevels(levels.filter(item => item.id !== id));
     await deleteLevel(id)
+
   }
 
 function editLevels(levels: iLevels) {
@@ -126,28 +130,20 @@ function editLevels(levels: iLevels) {
                            <Mytext style={textStyles.textBody}> Cor: {item.color}</Mytext>
                            <Mytext style={textStyles.textBody}> UserId: {item.userId}</Mytext>
                            
- 
-                            {/*
-                            <View style={styles.buttonsContainer}>
-                                <TouchableOpacity 
-                                style={styles.editButton}
-                                onPress={() => {editLevels(item.id)}}>
-
-                                    <Text style={styles.buttonText}>EDIT</Text>
-
-                                </TouchableOpacity>
-
-                                <TouchableOpacity 
-                                style={styles.delButton}
-                                onPress={() => {deleteLevels(item.id)}}>
-
-                                    <Text style={styles.buttonText}>DELETE</Text>
-
-                                </TouchableOpacity>
-                               
-                            </View>
-                            */}
-
+    {/*
+    <View style={styles.buttonsContainer}>
+    <TouchableOpacity 
+    style={styles.editButton}
+    onPress={() => {editLevels(item.id)}}>
+    <Text style={styles.buttonText}>EDIT</Text>
+    </TouchableOpacity>
+    <TouchableOpacity 
+    style={styles.delButton}
+    onPress={() => {deleteLevels(item.id)}}>
+    <Text style={styles.buttonText}>DELETE</Text>
+    </TouchableOpacity>             
+    </View>
+    */}
                         </MyItem>
 
                     )}
@@ -164,12 +160,15 @@ const styles = StyleSheet.create({
         flex: 1,
         padding: 20,
         backgroundColor: '#FFF'
-    },
+        
+},
     row: {
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'flex-start',
-    },
+
+},
+
     form: {
         flex: 1,
         padding: 20,
@@ -181,7 +180,9 @@ const styles = StyleSheet.create({
         shadowRadius: 5,
         marginRight: 10,
         minWidth: '45%',
-    },
+
+},
+
     listContainer: {
         flex: 1,
         padding: 20,
@@ -192,64 +193,83 @@ const styles = StyleSheet.create({
         shadowOffset: { width: 0, height: 4 },
         shadowRadius: 5,
         minWidth: '45%',
-    },
+
+},
+
     title: {
         fontSize: 22,
         fontWeight: 'bold',
         textAlign: 'center',
         marginBottom: 20,
-    },
+
+},
+
     subtitle: {
         fontSize: 18,
         fontWeight: 'bold',
         marginBottom: 10,
-    },
+
+},
+
     input: {
         borderWidth: 1,
         borderColor: '#ccc',
         borderRadius: 5,
         padding: 10,
         marginBottom: 10,
-    },
+
+},
+
     postItem: {
         padding: 10,
         marginVertical: 5,
         backgroundColor: '#f8f8f8',
         borderRadius: 5,
-    },
+
+},
+
     postText: {
         fontSize: 16,
         fontWeight: 'bold',
-    },
+
+},
+
     postUrl: {
         fontSize: 14,
         color: '#007BFF',
         marginBottom: 5,
-    },
+
+},
+
     buttonText:{
         color:'#000000',
         fontWeight: 'bold'
-    },
+
+},
+
     buttonsContainer:{
         flexDirection: 'row',
         alignItems: 'center',
         gap: 20,
         alignContent:'space-around',
-    },
+
+},
+
     editButton:{ backgroundColor:'#FFFF00',
         padding: 10,
         borderRadius: 5,
         alignItems: 'center',
         justifyContent:'center',
 
-    },
+},
+
     delButton:{ backgroundColor:'#f44336',
         padding: 10,
         borderRadius: 5,
         alignItems:'center',
         justifyContent:'center',
 
-    },
+},
 
     card: {
         backgroundColor: '#FFF',
@@ -262,7 +282,8 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.1,
         shadowOffset: { width: 0, height: 2 },
         shadowRadius: 3,
-      },
+
+},
 
       fundo: {
         height: 40,
@@ -272,6 +293,7 @@ const styles = StyleSheet.create({
         paddingHorizontal: 10,
         marginBottom: 10,
         backgroundColor: '#FFF',
-      },
+
+},
 
 });
