001fe38608b9b496e1ce3bf6a94bc9cda1fb1150
iris1518 <neuslinda12@gmail.com>
2025-04-22 22:08:39 -0300

    não terminado

--- src/components/MyNotify.tsx
+++ src/components/MyNotify.tsx
@@ -30,7 +30,7 @@ const MyNotify: React.FC<MyNotifyProps> = ({ style }) => {
       }
     }
     getTodos();
-  }, []);
+  }, [notifications]);
 
   return (
     <View>
@@ -82,7 +82,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#F3E5F5', // lilás claro
     borderRadius: 12,
     padding: 12,
-    maxHeight: 300,
+    height:"100vh",
     width: 320,
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 6 },
