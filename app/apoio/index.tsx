0744c9f8c0c15b18e74c8432c8381919b6b077a7
IgorBonfim0 <igor.bonfim.work@gmail.com>
2025-04-30 21:18:01 -0300

    Correção do caminho da imagem

--- app/apoio/index.tsx
+++ app/apoio/index.tsx
@@ -128,7 +128,7 @@ export default function HomeScreen() {
   return (
     <MyView router={router} style={{ flex: 1 }}>
       <ImageBackground
-        source={require('../../app/apoio/imagens/download.png')} // <-- sua imagem aqui!
+        source={require('../app/apoio/imagens/download.png')} // <-- sua imagem aqui!
         style={styles.backgroundImage}
         resizeMode='center'
       >
