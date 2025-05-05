979adb44c1e223e3f616201f10cb94a993100693
DanFerrari <danieldasilvaferrari@gmail.com>
2025-05-05 13:55:24 -0300

    feat: implement classroom data fetching and enhance user role management

--- app/classroom/index.tsx
+++ app/classroom/index.tsx
@@ -1,45 +1,105 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, ActivityIndicator } from "react-native";
-import { getUserRoles } from "@/src/controllers/classroom";
+import { View, Text, ActivityIndicator , Pressable} from "react-native";
+import { getUserRoles,getClassroomData } from "@/src/controllers/classroom";
 import MyView from "@/src/components/MyView";
 import { Icon } from "react-native-paper";
-
+import { Ionicons, FontAwesome5, Feather, MaterialIcons } from '@expo/vector-icons';
+import { useRouter } from "expo-router";
+import { getUserById ,iUser} from "@/src/controllers/users";
+import { ScrollView } from "react-native-gesture-handler";
 export default function HomeScreen() {
+  const router = useRouter();
   const [loading, setLoading] = useState(true);
   const [role, setRole] = useState<null | {
     isStudent: boolean;
     isEmployee: boolean;
     employeePosition?: string;
+    positionId?:number
+
   }>(null);
 
-  const userId =8;
+  const [classroomData, setClassroomData] = useState<null | {
+    numberClasses: number | null;
+    numberStudents: number | null;
+    numberTeachers: number | null;
+    numberDiciplines: number | null;
+  }>(null);
+
+  const [user, setUser] = useState<null | iUser>(null);
+
+
+
+  const userId = localStorage.getItem("userId");
+  
 
   useEffect(() => {
     const fetchRoles = async () => {
-      const roles = await getUserRoles(userId);
+      const roles = await getUserRoles(Number(userId));
       setRole(roles);
       setLoading(false);
+     
+    };
+    const fetchClassroomData = async () => {
+      const classroomData = await getClassroomData(Number(userId));
+      setClassroomData(classroomData);
+      setLoading(false);
+     
     };
 
+    const fetchUserData = async () => {
+      const userData = await getUserById(Number(userId));
+      setUser(userData.data);
+      setLoading(false);
+     
+    };
+
+    fetchClassroomData();
+
     fetchRoles();
+    fetchUserData();
   }, []);
 
-  const CardTop = ({ icon, title, description }: { icon: string; title: string; description: string }) => {
+
+
+  const Card = ({ icon, title, description }: { icon: string; title: string; description: string }) => {
     return (
-      <View className="bg-white shadow-md rounded-2xl p-4  sm:w-1/2 lg:w-1/4 flex flex-row gap-3 items-center" >
+      <View className="bg-white  rounded-2xl p-4   w-[256] h-[134] flex flex-row gap-3 items-center" >
         <View className="flex items-center justify-center bg-primary-20 w-10 h-10 rounded-lg">
         <Icon source={icon} size={22} color="#9128E2" />
-
         </View>
         <View className="flex flex-col  justify-start">
-        <Text className="text-xl font-bold text-gray-900 ">{title}</Text>
-        <Text className="text-sm font-medium text-left color-secondary-400">{description}</Text>
+          <Text  className="text-sm font-medium  text-left color-secondary-400">{description}</Text>
+          <Text className="text-xl font-bold text-gray-900 ">{title}</Text>
         </View>
       </View>
     );
   };
 
+  let turma:string = role?.isEmployee ? 'Turmas' : 'Turma'
+  let alunos:string = role?.isEmployee ? 'Alunos' : 'Meu desempenho'
+  const menuItems = [
+    
+  
+    { label: 'Home', icon: <Ionicons name="home" size={24} className="color-primary-100" />, route: 'classroom/index' },
+    { label: turma, icon:<MaterialIcons name="class" size={24} className="color-secondary-400" />, route: 'classroom/classes' },
+    { label: 'Cronograma', icon: <Ionicons name="calendar-sharp" size={24} className="color-secondary-400" />, route: 'classroom/timelines' },
+    { label: alunos, icon: <Ionicons name="school" size={24} className="color-secondary-400" />, route: 'classroom/students' },
+    
+  ];
 
+
+  const WelcomeMensage = () => {
+    if(role?.isEmployee){
+      return(
+        <View className="flex flex-row items-center justify-start bg-white mt-[51] px-10  w-full rounded-lg h-[114] ">
+         <Text className="text-2xl font-medium text-left color-secondary-500">Bem vindo de volta, <Text className="font-semibold">{user?.name}</Text>!</Text>          
+        </View>
+      )
+    }
+  }
+
+
+ 
 
 
 
@@ -54,13 +114,56 @@ export default function HomeScreen() {
 
   return (
     <MyView>
-      {role?.isEmployee && role.employeePosition === "Professor" && (
-        <View className="mb-6">
-          <Text className="text-xl font-bold text-blue-700 mb-2">Área do Professor</Text>
-          <Text className="text-gray-700">• Visualizar turmas</Text>
-          <Text className="text-gray-700">• Lançar notas</Text>
-        </View>
-      )}
+      <View className="flex flex-row h-full w-full bg-secondary-100">
+
+      
+ <View className="flex flex-col  h-full gap-14 w-[350] bg-white  items-center p-6 rounded-md">
+  {menuItems.map((item, index) => {
+    if (index === 0) {
+      return (
+        <Pressable
+          className="flex flex-row gap-3 items-center p-1 justify-start w-[200] bg-purple-100 rounded-lg hover:bg-purple-200"
+          key={index}
+          onPress={() => router.push(item.route)}
+        >
+          <View className="flex items-center justify-center w-10 h-10 rounded-lg">
+            {item.icon}
+          </View>
+          <Text className="font-medium color-primary-100">{item.label}</Text>
+        </Pressable>
+      );
+    } else {
+      return (
+        <Pressable
+          className="flex flex-row gap-3 items-center p-1 justify-start w-[200] bg-white rounded-lg hover:bg-gray-100"
+          key={index}
+          onPress={() => router.push(item.route)}
+        >
+          <View className="flex items-center justify-center w-10 h-10 rounded-lg">
+            {item.icon}
+          </View>
+          <Text className="font-medium color-secondary-400">{item.label}</Text>
+        </Pressable>
+      );
+    }
+  })}
+ </View>
+
+
+      {role?.isEmployee && role.positionId === 4 && (
+        <ScrollView className="h-full flex-1 bg-secondary-100 items-center ">
+          <WelcomeMensage/>
+          <View className="flex flex-row gap-3  p-4">
+            <Card icon="account-group" title={classroomData?.numberClasses?.toString() || "0"} description="Total de Turmas"/>
+            <Card icon="book" title={classroomData?.numberDiciplines?.toString() || "0"} description="Total de diciplinas"/>
+            <Card icon="school" title={classroomData?.numberStudents?.toString() || "0"} description="Total de Alunos"/>
+            <Card icon="star" title={ "0"} description="Pendencias"/>
+          </View>
+
+
+        </ScrollView>
+      )
+      }
 
       {role?.isStudent && (
         <View className="mb-6">
@@ -70,21 +173,22 @@ export default function HomeScreen() {
         </View>
       )}
 
-      {role?.isEmployee && role.employeePosition === "Admin" && (
+      {(role?.isEmployee && (role.positionId === 5 || role.positionId === 6)  ) && (
         <View className="mb-6">
           <Text className="text-xl font-bold text-gray-800 mb-2">Bem-vindo ao sistema</Text>
           <Text className="text-gray-600">Admin.</Text>
         </View>
       )}
 
-      {!role?.isStudent && !(role?.isEmployee && role.employeePosition === "Professor") && (
+      {(role?.isEmployee && (role.positionId !== 5 && role.positionId !== 6 && role.positionId !== 4)  )&& (
         <View>
 
           <Text className="text-xl font-bold text-gray-800 mb-2">Bem-vindo ao sistema</Text>
           <Text className="text-gray-600">Seu perfil ainda não tem uma função definida.</Text>
         </View>
       )}
-      <CardTop icon="school" title="12" description="Classes." />
+      </View>
+
     </MyView>
   );
 }
--- src/controllers/classroom.tsx
+++ src/controllers/classroom.tsx
@@ -4,12 +4,126 @@ export type UserRole = {
   isStudent: boolean;
   isEmployee: boolean;
   employeePosition?: string; 
+  positionId?:number
+
 };
+
+export type ClassRoomData = {
+  numberCourses: number | null;
+  numberStudents: number | null
+  numberTeachers: number | null
+  numberClasses: number | null
+  numberDiciplines: number | null
+
+}
+
+
+export async function getClassroomData(userId: number): Promise<ClassRoomData>{
+  let numberCourses: number | null = 0
+  let numberStudents: number | null = 0
+  let numberTeachers: number | null = 0
+  let numberClasses: number | null = 0
+  let numberDiciplines: number | null = 0
+  
+  const contarCourses = async () => {
+        const { count, error } = await supabase
+          .from('courses')
+          .select('*', { count: 'exact', head: true });
+      
+        if (error) {
+          console.error('Erro ao contar cursos:', error);
+          return 0;
+        }
+      
+        console.log('Total de cursos:', count);
+        return count;
+      };
+      const contarClasses = async () => {
+        const { count, error } = await supabase
+          .from('class')
+          .select('*', { count: 'exact', head: true });
+      
+        if (error) {
+          console.error('Erro ao contar turmas:', error);
+          return 0;
+        }
+      
+        console.log('Total de turmas:', count);
+        return count;
+      };
+
+
+
+      const contarStudents = async () => {
+        const { count, error } = await supabase
+          .from('students')
+          .select('*', { count: 'exact', head: true });
+      
+        if (error) {
+          console.error('Erro ao contar alunos:', error);
+          return 0;
+        }
+      
+        console.log('Total de alunos:', count);
+        return count;
+      };
+
+      const contarTeachers = async () => {
+        const { count, error } = await supabase
+          .from('employees')
+          .select('*', { count: 'exact', head: true })
+          .eq("positions_id",4);
+      
+        if (error) {
+          console.error('Erro ao contar professores:', error);
+          return 0;
+        }
+      
+        console.log('Total de professores:', count);
+        return count;
+      };
+
+
+      const contarDiciplines = async () => {
+        const { count, error } = await supabase
+          .from('disciplines')
+          .select('*', { count: 'exact', head: true });
+      
+        if (error) {
+          console.error('Erro ao contar disciplinas:', error);
+          return 0;
+        }
+      
+        console.log('Total de disciplinas:', count);
+        return count;
+      };
+
+    numberDiciplines = await contarDiciplines()
+    numberCourses = await contarCourses()
+    numberStudents = await contarStudents()
+    numberTeachers = await contarTeachers()
+    numberClasses = await contarClasses()
+  
+  
+  
+    return {
+    numberCourses,
+    numberStudents,
+    numberTeachers,
+    numberClasses,
+    numberDiciplines
+  }
+
+} 
+
 
 export async function getUserRoles(userId: number): Promise<UserRole> {
   let isStudent = false;
   let isEmployee = false;
   let employeePosition: string | undefined = undefined;
+  let positionId: number | undefined = undefined
+
+
 
   const { data: studentData, error:studentError } = await supabase.from("students")
     .select("id,user_id")
@@ -21,6 +135,10 @@ export async function getUserRoles(userId: number): Promise<UserRole> {
   }
 
   console.log("error",studentError)
+
+
+  
+
   
 
   const { data: employeeData } = await supabase
@@ -40,7 +158,9 @@ export async function getUserRoles(userId: number): Promise<UserRole> {
       .single();
 
     employeePosition = positionsData?.name;
+    positionId = employeeData.positions_id
+  
   }
 
-  return { isStudent, isEmployee, employeePosition };
+  return { isStudent, isEmployee, employeePosition, positionId, };
 }
